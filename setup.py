cc85ac7c81b82870498e30aabe1f973c70c93407
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-01-15 21:48:48 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import subprocess
 
 def git_version():
     cmd = 'git log --format="%h" -n 1'
-    return subprocess.check_output(shlex.split(cmd)).decode()
+    return subprocess.check_output(shlex.split(cmd)).decode().strip()
 
 
 version = git_version()
@@ -18,4 +18,7 @@ setup(
     install_requires=[],  # see requirements.txt
     author='Kentaro Wada',
     author_email='www.kentaro.wada@gmail.com',
+    license='MIT',
+    url='https://github.com/wkentaro/objslampp',
+    description='Volumetric fusion and CAD alignment for object-level SLAM',
 )
