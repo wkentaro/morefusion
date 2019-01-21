45c6d0e545113a38c058a13dcde45bc0bd988e6a
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-01-21 21:03:42 +0000

    Use ResNetFeatureExtractor in ycb_video.py

--- examples/ycb_video/ycb_video.py
+++ examples/ycb_video/ycb_video.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python
 
-import argparse
-
 import imgviz
 import numpy as np
 import termcolor
 
 import objslampp
 
+from tmp import ResNetFeatureExtractor
+
 
 class MainApp(object):
 
-    def __init__(self):
-        parser = argparse.ArgumentParser(
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        )
-        args = parser.parse_args()
-        del args
+    def __init__(self, res=False, gpu=0):
+        self._dataset = objslampp.datasets.YCBVideoDataset()
 
-        self.dataset = objslampp.datasets.YCBVideoDataset()
+        self._res = None
+        if res:
+            self._res = ResNetFeatureExtractor(gpu=gpu)
 
-    def run(self):
+        self._mainloop()
+
+    def _mainloop(self):
         index = 1000
-        imageset = self.dataset.imageset('train')
+        imageset = self._dataset.imageset('train')
 
         play = False
         while True:
             image_id = imageset[index]
             termcolor.cprint(f'[{index}] {image_id}', attrs={'bold': True})
 
-            frame = self.dataset.get_frame(image_id)
-            image = self.process_frame(frame)
+            frame = self._dataset.get_frame(image_id)
+            image = self._process_frame(frame)
 
             imgviz.io.cv_imshow(image, __file__)
 
@@ -53,7 +53,7 @@ class MainApp(object):
             elif key == 'q':
                 break
 
-    def process_frame(self, frame):
+    def _process_frame(self, frame):
         meta = frame['meta']
         color = frame['color']
 
@@ -78,9 +78,16 @@ class MainApp(object):
         vert_viz_y = imgviz.depth2rgb(vertmap[:, :, 1])
         vert_viz_z = imgviz.depth2rgb(vertmap[:, :, 2])
 
+        if self._res is None:
+            feat_viz = np.zeros_like(color)
+        else:
+            feat = self._res.extract_feature(color)
+            feat_viz = self._res.feature2rgb(feat, label != 0)
+
         roi_viz_color = []
         roi_viz_depth = []
         roi_viz_label = []
+        roi_viz_feat = []
         for bbox, mask in zip(bboxes, masks):
             y1, x1, y2, x2 = bbox.round().astype(int)
             mask_roi = mask[y1:y2, x1:x2]
@@ -90,12 +97,16 @@ class MainApp(object):
             depth_roi[~mask_roi] = 0
             label_roi = label_viz[y1:y2, x1:x2].copy()
             label_roi[~mask_roi] = 0
+            feat_roi = feat_viz[y1:y2, x1:x2].copy()
+            feat_roi[~mask_roi] = 0
             roi_viz_color.append(color_roi)
             roi_viz_depth.append(depth_roi)
             roi_viz_label.append(label_roi)
+            roi_viz_feat.append(feat_roi)
         roi_viz_color = imgviz.tile(roi_viz_color, border=(255, 255, 255))
         roi_viz_depth = imgviz.tile(roi_viz_depth, border=(255, 255, 255))
         roi_viz_label = imgviz.tile(roi_viz_label, border=(255, 255, 255))
+        roi_viz_feat = imgviz.tile(roi_viz_feat, border=(255, 255, 255))
 
         viz = imgviz.tile([
             color,
@@ -109,7 +120,7 @@ class MainApp(object):
             roi_viz_color,
             roi_viz_depth,
             roi_viz_label,
-            np.zeros_like(color),
+            roi_viz_feat,
         ], shape=(3, 4), border=(255, 255, 255))
         viz = imgviz.resize(viz, height=500)
 
@@ -117,4 +128,6 @@ class MainApp(object):
 
 
 if __name__ == '__main__':
-    MainApp().run()
+    import fire
+
+    fire.Fire(MainApp)
