2e4110893e6874a93c0c37bd8abba264e32c1411
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-01-25 13:03:04 +0000

    Add ycb.data for class_names and class_id_to_name

--- objslampp/datasets/__init__.py
+++ objslampp/datasets/__init__.py
@@ -1,5 +1,9 @@
 # flake8: noqa
 
+from . import ycb
+
 from .ycb_video import YCBVideoDataset
 
 from .ycb_video_models import YCBVideoModelsDataset
+
+from .ycb_video_multi_view_pose_estimation import YCBVideoMultiViewPoseEstimationDataset
---
+++ objslampp/datasets/ycb/__init__.py
@@ -0,0 +1,4 @@
+# flake8: noqa
+
+from .data import class_id_to_name
+from .data import class_names
