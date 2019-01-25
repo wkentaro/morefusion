afc24c56392aa55137a938bb7065ee43d95b7870
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-01-25 13:27:11 +0000

    Move funcs for cad_voxel_mapping to module

--- objslampp/geometry/__init__.py
+++ objslampp/geometry/__init__.py
@@ -3,6 +3,7 @@
 from .get_aabb_from_points import get_aabb_from_points
 from .get_homography_Rt import get_homography_Rt
 from .get_points_from_angles import get_points_from_angles
+from .get_uniform_points_on_sphere import get_uniform_points_on_sphere
 from .look_at import look_at
 from .pointcloud_from_depth import pointcloud_from_depth
 from .voxel_mapping import VoxelMapping
