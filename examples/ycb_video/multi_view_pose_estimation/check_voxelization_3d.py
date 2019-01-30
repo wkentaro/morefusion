e5b47cf555b0506beefa00f6669ab4d83b5c5f27
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-01-30 20:40:25 +0000

    Visualize nicely Voxelization3D

--- examples/ycb_video/multi_view_pose_estimation/check_voxelization_3d.py
+++ examples/ycb_video/multi_view_pose_estimation/check_voxelization_3d.py
@@ -2,13 +2,14 @@
 
 from chainer.backends import cuda
 import numpy as np
-import trimesh
 import pybullet  # NOQA
+import trimesh
+import trimesh.viewer
 
 import objslampp
 
 
-def check_voxelization_3d(gpu):
+def check_voxelization_3d(gpu, **kwargs):
     np.random.seed(0)
 
     dataset = objslampp.datasets.YCBVideoMultiViewPoseEstimationDataset(
@@ -38,19 +39,30 @@ def check_voxelization_3d(gpu):
     matrix_values = cuda.to_cpu(y.array)
     matrix_filled = (matrix_values != 0).any(axis=3)
 
+    scene = trimesh.Scene()
+    scene.angles = np.zeros(3)
+
     geom = trimesh.voxel.Voxel(
         matrix=matrix_filled, pitch=pitch, origin=origin
     ).as_boxes()
     I, J, K = zip(*np.argwhere(matrix_filled))
     geom.visual.face_colors = matrix_values[I, J, K].repeat(12, axis=0)
-    geom.show()
+    scene.add_geometry(geom)
+
+    def callback(scene):
+        scene.set_camera(angles=scene.angles)
+        scene.angles += [0, np.deg2rad(1), 0]
+
+    trimesh.viewer.SceneViewer(scene=scene, callback=callback, **kwargs)
 
 
 def main():
-    print('Running on CPU')
-    check_voxelization_3d(gpu=-1)
-    print('Running on GPU')
-    check_voxelization_3d(gpu=0)
+    check_voxelization_3d(
+        gpu=-1, start_loop=False, caption='Voxelization3D (CPU)'
+    )
+    check_voxelization_3d(
+        gpu=-1, caption='Voxelization3D (GPU)'
+    )
 
 
 if __name__ == '__main__':
