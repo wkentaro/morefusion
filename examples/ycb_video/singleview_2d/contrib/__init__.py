7ea32c477c8975f040976839cd59143f1b9bf1e2
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-03-27 16:41:42 +0000

    Refactor singleview_2d

--- examples/ycb_video/singleview_2d/contrib/__init__.py
+++ examples/ycb_video/singleview_2d/contrib/__init__.py
@@ -1,4 +1,7 @@
 # flake8: noqa
 
 from .dataset import Dataset
+
 from .model import Model
+
+from .render import render
--- examples/ycb_video/singleview_2d/evaluate.py
+++ examples/ycb_video/singleview_2d/evaluate.py
@@ -6,97 +6,160 @@ import pathlib
 import pprint
 
 import chainer
+from chainer import cuda
+import imgviz
 import matplotlib.pyplot as plt
 import pandas
+import trimesh.transformations as tf
 
 import objslampp
 
-from contrib import Model
-from contrib import Dataset
+import contrib
 
 
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-)
-parser.add_argument('model', help='model file in a log dir')
-parser.add_argument('--gpu', type=int, default=0, help='gpu id')
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument('model', help='model file in a log dir')
+    parser.add_argument('--gpu', type=int, default=0, help='gpu id')
+    parser.add_argument('--show', action='store_true', help='show')
+    args = parser.parse_args()
 
-args_file = pathlib.Path(args.model).parent / 'args'
-with open(args_file) as f:
-    args_data = json.load(f)
-pprint.pprint(args_data)
+    args_file = pathlib.Path(args.model).parent / 'args'
+    with open(args_file) as f:
+        args_data = json.load(f)
+    pprint.pprint(args_data)
 
-if args.gpu >= 0:
-    chainer.cuda.get_device_from_id(args.gpu).use()
+    if args.gpu >= 0:
+        chainer.cuda.get_device_from_id(args.gpu).use()
 
-model = Model(
-    n_fg_class=21,
-    freeze_until='none',
-)
-if args.gpu >= 0:
-    model.to_gpu()
+    model = contrib.Model(
+        n_fg_class=21,
+        freeze_until='none',
+    )
+    if args.gpu >= 0:
+        model.to_gpu()
 
-print('==> Loading trained model: {}'.format(args.model))
-chainer.serializers.load_npz(args.model, model)
-print('==> Done model loading')
+    print('==> Loading trained model: {}'.format(args.model))
+    chainer.serializers.load_npz(args.model, model)
+    print('==> Done model loading')
 
-dataset = Dataset('val', class_ids=[2])
+    dataset = contrib.Dataset('val', class_ids=[2])
 
-# -----------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
 
-observations = []
-for index in range(len(dataset)):
-    image_id = dataset.ids[index][0]
-    # if image_id.split('/')[0] != '0054':
-    #     continue
+    observations = []
+    for index in range(len(dataset)):
+        image_id, class_id = dataset.ids[index]
+        # if image_id.split('/')[0] != '0054':
+        #     continue
 
-    examples = dataset[index:index + 1]
-    inputs = chainer.dataset.concat_examples(examples, device=args.gpu)
-    with chainer.no_backprop_mode() and chainer.using_config('train', False):
-        quaternion_pred = model.predict(
-            class_id=inputs['class_id'],
-            rgb=inputs['rgb'],
-        )
-
-        reporter = chainer.Reporter()
-        reporter.add_observer('main', model)
-        observation = {}
-        with reporter.scope(observation):
-            model.evaluate(
+        examples = dataset[index:index + 1]
+        inputs = chainer.dataset.concat_examples(examples, device=args.gpu)
+        with chainer.no_backprop_mode() and \
+                chainer.using_config('train', False):
+            quaternion_pred = model.predict(
                 class_id=inputs['class_id'],
-                quaternion_true=inputs['quaternion_true'],
-                translation_true=inputs['translation_true'],
-                quaternion_pred=quaternion_pred,
-                translation_rough=inputs['translation_rough'],
+                rgb=inputs['rgb'],
             )
-        observations.append(observation)
 
-    print(f'[{index:08d}] [{image_id}] {observation}')
+            reporter = chainer.Reporter()
+            reporter.add_observer('main', model)
+            observation = {}
+            with reporter.scope(observation):
+                model.evaluate(
+                    class_id=inputs['class_id'],
+                    quaternion_true=inputs['quaternion_true'],
+                    translation_true=inputs['translation_true'],
+                    quaternion_pred=quaternion_pred,
+                    translation_rough=inputs['translation_rough'],
+                )
+            observations.append(observation)
 
-df = pandas.DataFrame(observations)
+        print(f'[{index:08d}] [{image_id}] {observation}')
 
-errors = df['main/add_rotation/0002'].values
-auc, x, y = objslampp.metrics.auc_for_errors(errors, 0.1, return_xy=True)
-print('auc (add_rotation):', auc)
-plt.subplot(121)
-plt.title('ADD (rotation) (AUC={:.1f})'.format(auc * 100))
-plt.plot(x, y)
-plt.xlim(0, 0.1)
-plt.ylim(0, 1)
-plt.xlabel('average distance threshold [m]')
-plt.ylabel('accuracy')
+        if not args.show:
+            continue
 
-plt.subplot(122)
-errors = df['main/add/0002'].values
-auc, x, y = objslampp.metrics.auc_for_errors(errors, 0.1, return_xy=True)
-print('auc (add):', auc)
-plt.title('ADD (rotation + translation) (AUC={:.1f})'.format(auc * 100))
-plt.plot(x, y)
-plt.xlim(0, 0.1)
-plt.ylim(0, 1)
-plt.xlabel('average distance threshold [m]')
-plt.ylabel('accuracy')
+        frame = dataset.get_frame(image_id)
+        rgb = frame['color']
+        meta = frame['meta']
+        K = meta['intrinsic_matrix']
+        cad_file = objslampp.datasets.YCBVideoModels()\
+            .get_model(class_id=class_id)['textured_simple']
 
-plt.tight_layout()
-plt.show()
+        quaternion_pred = cuda.to_cpu(quaternion_pred.array)[0]
+        translation_pred = cuda.to_cpu(inputs['translation_rough'])[0]
+        T_pred = tf.quaternion_matrix(quaternion_pred)
+        T_pred[:3, 3] = translation_pred
+        quaternion_true = cuda.to_cpu(inputs['quaternion_true'])[0]
+        translation_true = cuda.to_cpu(inputs['translation_true'])[0]
+        T_true = tf.quaternion_matrix(quaternion_true)
+        T_true[:3, 3] = translation_true
+
+        Ts = {'true': T_true, 'pred': T_pred}
+
+        vizs = []
+        for which in ['true', 'pred']:
+            rgb_rendered, mask_rendered = contrib.render(
+                cad_file, Ts[which], K, height=480, width=640
+            )
+            mask_rendered = imgviz.label2rgb(
+                mask_rendered, img=rgb, alpha=0.7
+            )
+            viz = imgviz.tile([rgb, mask_rendered, rgb_rendered], (1, 3))
+            viz = imgviz.resize(viz, width=1000)
+
+            font_size = 20
+            add = observation[f'main/add/{class_id:04d}']
+            add_rot = observation[f'main/add_rotation/{class_id:04d}']
+            text = f'[{which}]: add={add * 100:.1f}cm, add_rot={add_rot * 100:.1f}cm' # NOQA
+            size = imgviz.draw.text_size(text, font_size)
+            viz = imgviz.draw.rectangle(
+                viz,
+                (0, 0),
+                (size[0] + 1, size[1] + 1),
+                color=(0, 255, 0),
+                fill=(0, 255, 0),
+            )
+            viz = imgviz.draw.text(viz, (1, 1), text, (0, 0, 0), font_size)
+            vizs.append(viz)
+        viz = imgviz.tile(vizs, (2, 1), border=(0, 0, 0))
+        yield viz
+
+    df = pandas.DataFrame(list(observations))
+
+    errors = df['main/add_rotation/0002'].values
+    auc, x, y = objslampp.metrics.auc_for_errors(errors, 0.1, return_xy=True)
+    print('auc (add_rotation):', auc)
+    plt.subplot(121)
+    plt.title('ADD (rotation) (AUC={:.1f})'.format(auc * 100))
+    plt.plot(x, y)
+    plt.xlim(0, 0.1)
+    plt.ylim(0, 1)
+    plt.xlabel('average distance threshold [m]')
+    plt.ylabel('accuracy')
+
+    plt.subplot(122)
+    errors = df['main/add/0002'].values
+    auc, x, y = objslampp.metrics.auc_for_errors(errors, 0.1, return_xy=True)
+    print('auc (add):', auc)
+    plt.title('ADD (rotation + translation) (AUC={:.1f})'.format(auc * 100))
+    plt.plot(x, y)
+    plt.xlim(0, 0.1)
+    plt.ylim(0, 1)
+    plt.xlabel('average distance threshold [m]')
+    plt.ylabel('accuracy')
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    vizs = main()
+    try:
+        imgviz.io.pyglet_imshow(vizs)
+        imgviz.io.pyglet_run()
+    except StopIteration:
+        pass
