c9bc0ffd04ad9ccdc5b38a017e083fee8eca0829
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-04-01 18:51:26 +0100

    extents and thickness as argument

--- examples/ycb_video/synthetic_data/contrib/simulation/scene_generation/bin_type.py
+++ examples/ycb_video/synthetic_data/contrib/simulation/scene_generation/bin_type.py
@@ -1,3 +1,4 @@
+import numbers
 import pathlib
 
 import numpy as np
@@ -12,24 +13,38 @@ from ... import extra
 
 class BinTypeSceneGeneration(SceneGenerationBase):
 
+    def __init__(self, *args, **kwargs):
+        extents = kwargs.pop('extents', (0.5, 0.5, 0.2))
+        assert len(extents) == 3
+        assert all(isinstance(x, numbers.Number) for x in extents)
+        self._extents = np.asarray(extents, dtype=float)
+
+        thickness = kwargs.pop('thickness', 0.01)
+        assert isinstance(thickness, numbers.Number)
+        self._thickness = thickness
+
+        super().__init__(*args, **kwargs)
+
     def init_space(self):
-        extents = np.array([0.5, 0.5, 0.2])
-        cad = extra.trimesh.bin_model(extents=extents, thickness=0.01)
+        cad = extra.trimesh.bin_model(
+            extents=self._extents,
+            thickness=self._thickness,
+        )
 
         cache_dir = pathlib.Path('/tmp')
         cad_file = cache_dir / f'{cad.md5()}.obj'
         if not cad_file.exists():
             trimesh.exchange.export.export_mesh(cad, str(cad_file))
 
-        container_id = objslampp.extra.pybullet.add_model(
+        unique_id = objslampp.extra.pybullet.add_model(
             visual_file=cad_file,
             collision_file=self._get_collision_file(cad_file),
-            position=(0, 0, extents[2] / 2),
+            position=(0, 0, self._extents[2] / 2),
         )
         self._simulate(nstep=100)
 
-        aabb_min, aabb_max = pybullet.getAABB(container_id)
+        aabb_min, aabb_max = pybullet.getAABB(unique_id)
         aabb_min, aabb_max = self._shrink_aabb(aabb_min, aabb_max, ratio=0.1)
 
-        self._objects[container_id] = dict(class_id=0)
+        self._objects[unique_id] = dict(class_id=0)
         self._aabb = aabb_min, aabb_max
