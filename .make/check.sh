1580fb25bc882a23daf89ac80f9148ca60f53e3b
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-04-01 01:08:27 +0100

    Sort check files

--- .make/check.sh
+++ .make/check.sh
@@ -16,7 +16,7 @@ source .anaconda3/bin/activate
 
 set -e
 
-for file in $(find $ROOT/checks -name '*.py'); do
+for file in $(find $ROOT/checks -name '*.py' | sort); do
   echo_bold "==> $file"
   python $file
 done
