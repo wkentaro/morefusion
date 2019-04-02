e44a0757553908d365db0e3810b18109921b033e
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-04-02 18:33:36 +0100

    Improve view_generated_data.py

--- examples/ycb_video/synthetic_data/view_generated_data.py
+++ examples/ycb_video/synthetic_data/view_generated_data.py
@@ -50,12 +50,26 @@ if __name__ == '__main__':
             print(f'[{i:08d}] [{dataset.ids[i]}]')
             example = dataset[i]
             rgb = example['rgb']
+
+            instance_index = np.where(example['class_ids'] == 2)[0][0]
+            T_cad2cam = example['Ts_cad2cam'][instance_index]
+            class_id = example['class_ids'][instance_index]
+
+            cad_file = objslampp.datasets.YCBVideoModels().get_cad_model(class_id)
+            rgb_rend, _, mask_rend = objslampp.extra.pybullet.render(
+                cad_file, T_cad2cam, fovy=45, height=480, width=640
+            )
+            mask_rend = imgviz.label2rgb(mask_rend, rgb)
+
             img = imgviz.tile([
                 rgb,
                 self._depth2rgb(example['depth']),
                 imgviz.label2rgb(example['instance_label'] + 1, rgb),
                 imgviz.label2rgb(example['class_label'], rgb),
-            ], border=(255, 255, 255))
+                rgb_rend,
+                mask_rend,
+            ], (2, 3), border=(255, 255, 255))
+            img = imgviz.resize(img, height=1000)
             return img
 
     imgviz.io.pyglet_imshow(Images(dataset))
