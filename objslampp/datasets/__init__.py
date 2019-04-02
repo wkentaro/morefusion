1876afb01e7ec7d3a677d1838f24606166762156
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-04-02 17:22:03 +0100

    Add DatasetBase to __init__

--- objslampp/datasets/__init__.py
+++ objslampp/datasets/__init__.py
@@ -2,6 +2,8 @@
 
 from . import ycb_video
 
+from .base import DatasetBase
+
 from .ycb_video import YCBVideoDataset
 from .ycb_video import YCBVideoModels
 from .ycb_video import YCBVideoSyntheticDataset
