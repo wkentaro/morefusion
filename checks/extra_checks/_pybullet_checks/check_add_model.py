4ba4220141c3f5823582f83a42141be858283dad
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-04-04 22:06:39 +0100

    Add check_add_model.py

---
+++ checks/extra_checks/_pybullet_checks/check_add_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import time
+
+import pybullet
+
+import objslampp
+
+
+objslampp.extra.pybullet.init_world()
+
+models = objslampp.datasets.YCBVideoModels()
+cad_file = models.get_cad_model(class_id=2)
+
+objslampp.extra.pybullet.add_model(
+    visual_file=cad_file,
+    position=(0, 0, 0.3),
+)
+for _ in range(1000):
+    pybullet.stepSimulation()
+
+print('unique_ids:', objslampp.extra.pybullet.unique_ids)
+
+for _ in range(3):
+    time.sleep(1)
+
+objslampp.extra.pybullet.del_world()
