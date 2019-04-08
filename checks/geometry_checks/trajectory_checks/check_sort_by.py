70393e6e79d3a843fc58b0cd49e62c49d28422e3
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-04-08 13:53:13 +0100

    Move objslampp.geometry.trajectory to upstream

---
+++ checks/geometry_checks/trajectory_checks/check_sort_by.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import numpy as np
+import trimesh
+import trimesh.transformations as tf
+
+import objslampp
+
+
+n_keypoints = 16
+n_points = 128
+
+# target
+aabb_min_target = (-0.2, -0.2, -0.2)
+aabb_max_target = (0.2, 0.2, 0.2)
+targets = np.random.uniform(
+    aabb_min_target, aabb_max_target, (n_keypoints, 3)
+)
+targets = objslampp.geometry.trajectory.sort(targets)
+targets = objslampp.geometry.trajectory.interpolate(targets, n_points=n_points)
+
+# eye
+aabb_min_eye = (-1, -1, -1)
+aabb_max_eye = (1, 1, 1)
+distance = np.full((n_keypoints,), 1, dtype=float)
+elevation = np.random.uniform(30, 90, (n_keypoints,))
+azimuth = np.random.uniform(0, 360, (n_keypoints,))
+eyes = objslampp.geometry.points_from_angles(distance, elevation, azimuth)
+indices = indices = np.linspace(0, 127, num=len(eyes))
+indices = indices.round().astype(int)
+eyes = objslampp.geometry.trajectory.sort_by(eyes, key=targets[indices])
+eyes = objslampp.geometry.trajectory.interpolate(eyes, n_points=128)
+
+# -----------------------------------------------------------------------------
+
+scene = trimesh.Scene()
+
+box = objslampp.extra.trimesh.wired_box((2, 2, 2))
+scene.add_geometry(box)
+
+axis = trimesh.creation.axis(0.01)
+point = trimesh.creation.icosphere(radius=0.01, color=(1., 0, 0))
+for eye, target in zip(eyes, targets):
+    transform = tf.translation_matrix(eye)
+    scene.add_geometry(axis, transform=transform)
+
+    transform = tf.translation_matrix(target)
+    scene.add_geometry(point, transform=transform)
+
+    ray = trimesh.load_path([eye, target])
+    scene.add_geometry(ray)
+
+objslampp.extra.trimesh.show_with_rotation(scene, resolution=(400, 400))
---
+++ checks/geometry_checks/trajectory_checks/check_sort_interpolate.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import numpy as np
+import trimesh
+import trimesh.transformations as tf
+
+import objslampp
+
+
+aabb_min = (-1, -1, -1)
+aabb_max = (1, 1, 1)
+
+keypoints = np.random.uniform(aabb_min, aabb_max, (16, 3))
+keypoints = objslampp.geometry.trajectory.sort(keypoints)
+
+trajectory = objslampp.geometry.trajectory.interpolate(keypoints, n_points=128)
+
+# -----------------------------------------------------------------------------
+
+scene = trimesh.Scene()
+
+box = objslampp.extra.trimesh.wired_box((2, 2, 2))
+scene.add_geometry(box)
+
+axis = trimesh.creation.axis(0.01, origin_color=(0, 0, 0))
+for point in keypoints:
+    transform = tf.translation_matrix(point)
+    scene.add_geometry(axis, transform=transform)
+
+axis = trimesh.creation.axis(0.01)
+for point in trajectory:
+    transform = tf.translation_matrix(point)
+    scene.add_geometry(axis, transform=transform)
+
+objslampp.extra.trimesh.show_with_rotation(scene)
