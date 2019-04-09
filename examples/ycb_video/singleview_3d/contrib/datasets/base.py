859af37951cd3476dc9a7768b2e7cebf317978f6
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-04-10 05:34:37 +0900

    CADOnlyDataset for singleview_3d

--- examples/ycb_video/singleview_3d/contrib/datasets/base.py
+++ examples/ycb_video/singleview_3d/contrib/datasets/base.py
@@ -1,3 +1,5 @@
+import imgaug
+import imgaug.augmenters as iaa
 import numpy as np
 
 import objslampp
@@ -7,6 +9,9 @@ class DatasetBase(objslampp.datasets.DatasetBase):
 
     voxel_dim = 32
     _cache_pitch = {}
+
+    _backgrounds = objslampp.datasets.YCBVideoDataset('train')
+    _occlusions = _backgrounds
 
     def _get_invalid_data(self):
         return dict(
@@ -30,3 +35,161 @@ class DatasetBase(objslampp.datasets.DatasetBase):
 
         self._cache_pitch[class_id] = pitch
         return pitch
+
+    def _augment(self, rgb, depth):
+        if 'rgb' in self._augmentation:
+            rgb = self._augment_rgb(rgb)
+
+        if 'depth' in self._augmentation:
+            depth = self._augment_depth(depth)
+
+        if 'occl' in self._augmentation:
+            rgb, depth = self._augment_occlusion(rgb, depth)
+
+        if 'segm' in self._augmentation:
+            rgb, depth = self._augment_segmentation(rgb, depth)
+
+        mask = ~np.isnan(depth)
+        rgb[~mask] = 0
+        depth[~mask] = np.nan
+        return rgb, depth
+
+    def _augment_rgb(self, rgb):
+        augmenter = iaa.Sequential([
+            iaa.ContrastNormalization(alpha=(0.8, 1.2)),
+            iaa.WithColorspace(
+                to_colorspace='HSV',
+                from_colorspace='RGB',
+                children=iaa.Sequential([
+                    iaa.WithChannels(
+                        (1, 2),
+                        iaa.Multiply(mul=(0.8, 1.2), per_channel=True),
+                    ),
+                    iaa.WithChannels(
+                        (0,),
+                        iaa.Multiply(mul=(0.95, 1.05), per_channel=True),
+                    ),
+                ]),
+            ),
+            iaa.GaussianBlur(sigma=(0, 1.0)),
+            iaa.KeepSizeByResize(children=iaa.Resize((0.25, 1.0))),
+        ])
+        rgb = augmenter.augment_image(rgb)
+
+        return rgb
+
+    def _augment_depth(self, depth):
+        depth = depth.copy()
+        random_state = imgaug.current_random_state()
+        depth += random_state.normal(scale=0.01, size=depth.shape)
+        return depth
+
+    def _augment_segmentation(self, rgb, depth):
+        rgb = rgb.copy()
+        depth = depth.copy()
+
+        random_state = imgaug.current_random_state()
+
+        index = random_state.randint(0, len(self._backgrounds))
+        background = self._backgrounds[index]
+        rgb_bg = background['color'].copy()
+        depth_bg = background['depth'].copy()
+
+        mask = ~np.isnan(depth)
+        bbox = objslampp.geometry.masks_to_bboxes(mask)
+        by1, bx1, by2, bx2 = bbox.round().astype(int)
+        bh, bw = by2 - by1, bx2 - bx1
+
+        cy, cx = random_state.uniform(
+            (bh, bw), (rgb_bg.shape[0] - bh, rgb_bg.shape[1] - bw)
+        ).round().astype(int)
+
+        y1 = int(round(cy - bh / 2))
+        x1 = int(round(cx - bw / 2))
+        y2 = y1 + bh
+        x2 = x1 + bw
+        mask_roi = mask[by1:by2, bx1:bx2]
+        rgb_bg[y1:y2, x1:x2][mask_roi] = rgb[by1:by2, bx1:bx2][mask_roi]
+        depth_bg[y1:y2, x1:x2][mask_roi] = depth[by1:by2, bx1:bx2][mask_roi]
+
+        mask = mask_roi
+        rgb = rgb_bg[y1:y2, x1:x2]
+        depth = depth_bg[y1:y2, x1:x2]
+
+        # randomly shift mask (like annotations in ycb_video)
+        y0, x0 = random_state.normal(
+            loc=10, scale=3.0, size=(2,)
+        ).round().astype(int)
+        y0, x0 = np.clip((y0, x0), 0, 20)
+        mask = np.pad(mask, 10, mode='constant', constant_values=0)[
+            y0:y0 + mask.shape[0], x0:x0 + mask.shape[1]
+        ]
+
+        rgb[~mask] = 0
+        depth[~mask] = np.nan
+        return rgb, depth
+
+    def _augment_occlusion(self, rgb, depth):
+        random_state = imgaug.current_random_state()
+        n_sample = random_state.randint(0, 3)
+        min_ratio_occl = 0.3
+        mask_old = ~np.isnan(depth)
+        for _ in range(n_sample):
+            rgb_new, depth_new = self._augment_occlusion_one(rgb, depth)
+            mask_new = ~np.isnan(depth_new)
+            ratio_occl_current = mask_new.sum() / mask_old.sum()
+            if ratio_occl_current < min_ratio_occl:
+                continue
+            rgb, depth = rgb_new, depth_new
+        return rgb, depth
+
+    def _augment_occlusion_one(self, rgb, depth):
+        rgb = rgb.copy()
+        depth = depth.copy()
+
+        mask = ~np.isnan(depth)
+
+        random_state = imgaug.current_random_state()
+
+        index = random_state.randint(0, len(self._backgrounds))
+        occlusion = self._occlusions[index]
+        rgb_occl = occlusion['color']
+        labels = np.unique(occlusion['label'])
+        labels = labels[labels > 0]
+        mask_occl = occlusion['label'] == random_state.choice(labels)
+
+        bbox = objslampp.geometry.masks_to_bboxes(mask_occl)
+        by1, bx1, by2, bx2 = bbox.round().astype(int)
+        bh, bw = by2 - by1, bx2 - bx1
+
+        rgb_occl = rgb_occl[by1:by2, bx1:bx2]
+        mask_occl = mask_occl[by1:by2, bx1:bx2]
+
+        H, W = rgb.shape[:2]
+        cy, cx = random_state.uniform(
+            (0, 0), rgb.shape[:2]
+        ).round().astype(int)
+
+        y1 = np.clip(int(round(cy - bh / 2)), 0, H)
+        x1 = np.clip(int(round(cx - bw / 2)), 0, W)
+        y2 = np.clip(y1 + bh, 0, H)
+        x2 = np.clip(x1 + bw, 0, W)
+
+        if (y2 - y1) == rgb_occl.shape[0]:
+            y1_occl = 0
+        else:
+            y1_occl = random_state.randint(0, rgb_occl.shape[0] - (y2 - y1))
+        if (x2 - x1) == rgb_occl.shape[1]:
+            x1_occl = 0
+        else:
+            x1_occl = random_state.randint(0, rgb_occl.shape[1] - (x2 - x1))
+        y2_occl = y1_occl + (y2 - y1)
+        x2_occl = x1_occl + (x2 - x1)
+        slice_occl = slice(y1_occl, y2_occl), slice(x1_occl, x2_occl)
+        mask_occl = mask_occl[slice_occl]
+        rgb[y1:y2, x1:x2][mask_occl] = rgb_occl[slice_occl][mask_occl]
+        mask[y1:y2, x1:x2][mask_occl] = 0
+
+        rgb[~mask] = 0
+        depth[~mask] = np.nan
+        return rgb, depth
--- examples/ycb_video/singleview_3d/contrib/datasets/cad_only.py
+++ examples/ycb_video/singleview_3d/contrib/datasets/cad_only.py
@@ -1,8 +1,6 @@
 import imgaug
-import imgaug.augmenters as iaa
 import imgviz
 import numpy as np
-import skimage.morphology
 import trimesh
 import trimesh.transformations as tf
 
@@ -34,118 +32,6 @@ class CADOnlyDataset(DatasetBase):
     def __len__(self):
         return self._n_sample
 
-    def _augment(self, rgb, depth):
-        if 'rgb' in self._augmentation:
-            rgb = self._augment_rgb(rgb)
-
-        if 'depth' in self._augmentation:
-            depth = self._augment_depth(depth)
-
-        if 'segm' in self._augmentation:
-            rgb, depth = self._augment_segmentation(rgb, depth)
-
-        if 'occl' in self._augmentation:
-            rgb, depth = self._augment_occlusion(rgb, depth)
-
-        mask = ~np.isnan(depth)
-        rgb[~mask] = 0
-        depth[~mask] = np.nan
-        return rgb, depth
-
-    def _partial_binary_dilation(self, mask):
-        random_state = imgaug.current_random_state()
-        selem = skimage.morphology.disk(random_state.randint(1, 10))
-        mask_dilated = skimage.morphology.binary_dilation(mask, selem)
-
-        augmenter = iaa.CoarseDropout(p=(0.5, 0.9), size_px=(10, 30))
-        mask_change = np.ones_like(mask)
-        mask_change = augmenter.augment_image(
-            mask_change.astype(np.uint8)
-        ).astype(bool)
-
-        mask_dst = mask.copy()
-        mask_dst[mask_change] = mask_dilated[mask_change]
-        return mask_dst
-
-    def _augment_segmentation(self, rgb, depth):
-        random_state = imgaug.current_random_state()
-
-        mask = ~np.isnan(depth)
-        rgb[~mask] = random_state.randint(0, 255, (np.sum(~mask), 3))
-        # noise to background depth
-        depth_copy = depth.copy()
-        height, width = depth_copy.shape[:2]
-        height_dst = int(round(height * 1.1))
-        depth_copy = imgviz.resize(depth_copy, height=height_dst)
-        y1 = int(round((height_dst - height) / 2))
-        x1 = int(round((depth_copy.shape[1] - width) / 2))
-        depth_copy = depth_copy[y1:y1 + height, x1:x1 + width]
-        depth_copy += random_state.normal(scale=0.05, size=depth_copy.shape)
-        depth[~mask] = depth_copy[~mask]
-
-        # noise to mask
-        mask = self._partial_binary_dilation(mask)
-        augmenter = iaa.Sometimes(
-            0.5,
-            iaa.ElasticTransformation(alpha=(0, 70.0), sigma=8.0)
-        )
-        mask = augmenter.augment_image(mask.astype(float)) > 0.5
-
-        rgb[~mask] = 0
-        depth[~mask] = np.nan
-        return rgb, depth
-
-    def _augment_depth(self, depth):
-        random_state = imgaug.current_random_state()
-        depth += random_state.normal(scale=0.01, size=depth.shape)
-        return depth
-
-    def _augment_occlusion(self, rgb, depth):
-        mask = ~np.isnan(depth)
-
-        augmenter = iaa.Sequential([
-            iaa.CoarseDropout(p=(0, 0.5), size_px=2, min_size=2),
-            iaa.ElasticTransformation(alpha=(0, 50), sigma=5),
-        ])
-
-        mask_org = mask.copy()
-        min_p = 0.5
-        while True:
-            mask = augmenter.augment_image(
-                mask_org.astype(np.uint8)
-            ).astype(bool)
-            mask = mask & mask_org
-            if (mask.sum() / mask_org.sum()) > min_p:
-                break
-
-        rgb[~mask] = 0
-        depth[~mask] = np.nan
-        return rgb, depth
-
-    def _augment_rgb(self, rgb):
-        augmenter = iaa.Sequential([
-            iaa.ContrastNormalization(alpha=(0.8, 1.2)),
-            iaa.WithColorspace(
-                to_colorspace='HSV',
-                from_colorspace='RGB',
-                children=iaa.Sequential([
-                    iaa.WithChannels(
-                        (1, 2),
-                        iaa.Multiply(mul=(0.8, 1.2), per_channel=True),
-                    ),
-                    iaa.WithChannels(
-                        (0,),
-                        iaa.Multiply(mul=(0.95, 1.05), per_channel=True),
-                    ),
-                ]),
-            ),
-            iaa.GaussianBlur(sigma=(0, 1.0)),
-            iaa.KeepSizeByResize(children=iaa.Resize((0.25, 1.0))),
-        ])
-        rgb = augmenter.augment_image(rgb)
-
-        return rgb
-
     @property
     def camera(self):
         return trimesh.scene.Camera(resolution=(320, 320), fov=(60, 60))
@@ -160,9 +46,11 @@ class CADOnlyDataset(DatasetBase):
     def get_example(self, index):
         random_state = imgaug.current_random_state()
 
+        # prepare class_id and cad_file
         class_id = random_state.choice(self._class_ids)
         cad_file = self._models.get_cad_model(class_id=class_id)
 
+        # get frame
         eye = objslampp.geometry.points_from_angles(
             distance=0.3,
             elevation=random_state.uniform(-90, 90),
@@ -183,23 +71,33 @@ class CADOnlyDataset(DatasetBase):
             height=self.camera.resolution[1],
             width=self.camera.resolution[0],
         )
+        mask = ~np.isnan(depth)
+        rgb[~mask] = 0
+        depth[~mask] = np.nan
 
-        if self._augmentation:
-            rgb, depth = self._augment(rgb, depth)
-
+        # keep rgb and index for get_frame
         self._rgb = index, rgb
 
+        # augment
+        if self._augmentation:
+            rgb, depth = self._augment(rgb, depth)
+            mask = ~np.isnan(depth)
+
+        # get point cloud
         K = self.camera.K
         pcd = objslampp.geometry.pointcloud_from_depth(
             depth, fx=K[0, 0], fy=K[1, 1], cx=K[0, 2], cy=K[1, 2],
         )
 
-        mask = ~np.isnan(depth)
+        # crop
         bbox = objslampp.geometry.masks_to_bboxes(mask)
         y1, x1, y2, x2 = bbox.round().astype(int)
+        if (y2 - y1) * (x2 - x1) == 0:
+            return self._get_invalid_data()
         rgb = rgb[y1:y2, x1:x2]
         pcd = pcd[y1:y2, x1:x2]
 
+        # finalize
         rgb = imgviz.centerize(rgb, (256, 256))
         pcd = imgviz.centerize(pcd, (256, 256), np.nan)
 
