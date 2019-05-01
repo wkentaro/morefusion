abfd3e85ff39f0a88e584260359b8f67339fc223
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-05-01 17:19:09 +0100

    Refactor align_occupancy_grids.py

--- examples/ycb_video/preliminary/align_occupancy_grids.py
+++ examples/ycb_video/preliminary/align_occupancy_grids.py
@@ -11,6 +11,8 @@ import trimesh.transformations as tf
 
 import objslampp
 
+import contrib
+
 from build_occupancy_grid import get_instance_grid
 
 
@@ -80,7 +82,7 @@ class OccupancyGridAlignmentModel(chainer.Link):
         return loss
 
 
-class RegisterationOccupancyGrid:
+class InstanceOccupancyGridRegistration:
 
     def __init__(
         self,
@@ -146,7 +148,7 @@ class RegisterationOccupancyGrid:
             self.step()
             yield self.transform
 
-    def visualize(self, cad, transform_true, transform_pred):
+    def visualize(self, cad, T_cad2cam_true, T_cad2cam_pred, T_com2cam):
         scenes = {}
 
         grid_target = self._grid_target
@@ -154,10 +156,13 @@ class RegisterationOccupancyGrid:
         pitch = self._pitch
         origin = self._origin
 
+        origin = tf.transform_points([origin], T_com2cam)[0]
+
         scene = trimesh.Scene()
         # occupied target/untarget
         voxel = trimesh.voxel.Voxel(
-            matrix=grid_target == id_target, pitch=pitch, origin=origin)
+            matrix=grid_target == id_target, pitch=pitch, origin=origin
+        )
         geom = voxel.as_boxes((1., 0, 0, 0.5))
         scene.add_geometry(geom, geom_name='occupied_target')
         voxel = trimesh.voxel.Voxel(
@@ -167,35 +172,40 @@ class RegisterationOccupancyGrid:
         )
         geom = voxel.as_boxes((0, 1., 0, 0.5))
         scene.add_geometry(geom, geom_name='occupied_untarget')
-        # bbox
-        aabb_min = origin
-        aabb_max = aabb_min + pitch * np.array(grid_target.shape)
-        geom = trimesh.path.creation.box_outline(aabb_max - aabb_min)
-        geom.apply_translation((aabb_min + aabb_max) / 2)
-        scene.add_geometry(geom)
         scenes['occupied'] = scene
 
         # empty
         scene = trimesh.Scene()
         voxel = trimesh.voxel.Voxel(
-            matrix=grid_target == 254, pitch=pitch, origin=origin)
+            matrix=grid_target == 254, pitch=pitch, origin=origin
+        )
         geom = voxel.as_boxes((0.5, 0.5, 0.5, 0.5))
         scene.add_geometry(geom, geom_name='empty')
         scenes['empty'] = scene
 
         scene = trimesh.Scene()
-        # cad_pred
-        cad_copy = cad.copy()
-        cad_copy.apply_transform(transform_pred)
-        scene.add_geometry(cad_copy, geom_name='cad_pred')
-        scenes['occupied'].add_geometry(cad_copy, geom_name='cad_pred')
-        scenes['empty'].add_geometry(cad_copy, geom_name='cad_pred')
         # cad_true
         cad_copy = cad.copy()
         cad_copy.visual.vertex_colors[:, 3] = 127
-        cad_copy.apply_transform(transform_true)
+        cad_copy.apply_transform(T_cad2cam_true)
         scene.add_geometry(cad_copy, geom_name='cad_true')
         scenes['cad'] = scene
+
+        # cad_pred
+        cad_copy = cad.copy()
+        cad_copy.apply_transform(T_cad2cam_pred)
+        scenes['occupied'].add_geometry(cad_copy, geom_name='cad_pred')
+        scenes['empty'].add_geometry(cad_copy, geom_name='cad_pred')
+        scenes['cad'].add_geometry(cad_copy, geom_name='cad_pred')
+
+        # bbox
+        aabb_min = origin - pitch / 2
+        aabb_max = aabb_min + pitch * np.array(grid_target.shape)
+        geom = trimesh.path.creation.box_outline(aabb_max - aabb_min)
+        geom.apply_translation((aabb_min + aabb_max) / 2)
+        scenes['occupied'].add_geometry(geom, geom_name='bbox')
+        scenes['empty'].add_geometry(geom, geom_name='bbox')
+        scenes['cad'].add_geometry(geom, geom_name='bbox')
 
         center = scenes['cad'].centroid
         for scene in scenes.values():
@@ -207,73 +217,146 @@ class RegisterationOccupancyGrid:
         return scenes
 
 
+class SceneOccupancyGridRegistration:
+
+    _models = objslampp.datasets.YCBVideoModels()
+
+    connectivity = 2  # threshold for occupancy grid voxelization
+
+    def __init__(
+        self,
+        instance_ids,
+        class_ids,
+        Ts_cad2cam_true,
+        pcd,
+        instance_label,
+    ):
+        N = len(instance_ids)
+        assert instance_ids.shape == (N,) and 0 not in instance_ids
+        assert class_ids.shape == (N,) and 0 not in class_ids
+        assert Ts_cad2cam_true.shape == (N, 4, 4)
+        H, W = pcd.shape[:2]
+        assert pcd.shape == (H, W, 3)
+        assert instance_label.shape == (H, W)
+
+        self._class_ids = class_ids
+        self._instance_ids = instance_ids
+        self._Ts_cad2cam_true = Ts_cad2cam_true
+        self._pcd = pcd
+        self._instance_label = instance_label
+        self._octrees = self.build_octrees(0.01)
+
+    def build_octrees(self, pitch):
+        pcd = self._pcd
+        instance_ids = self._instance_ids
+        instance_label = self._instance_label
+
+        nonnan = ~np.isnan(pcd).any(axis=2)
+        octrees = {}
+        for ins_id in np.r_[0, instance_ids]:
+            mask = instance_label == ins_id
+            octree = octomap.OcTree(pitch)
+            octree.insertPointCloud(
+                pcd[mask & nonnan],
+                np.array([0, 0, 0], dtype=float),
+            )
+            octrees[ins_id] = octree
+
+        return octrees
+
+    def __call__(self, instance_id):
+        models = self._models
+
+        # parameters
+        connectivity = 2
+        dim = 16  # 20
+        step = 100
+
+        # scene-level data
+        class_ids = self._class_ids
+        instance_ids = self._instance_ids
+        pcd = self._pcd
+        instance_label = self._instance_label
+        Ts_cad2cam_true = self._Ts_cad2cam_true
+        octrees = self._octrees
+
+        # instance-level data
+        index = np.where(instance_ids == instance_id)[0][0]
+        instance_id = instance_ids[index]
+        class_id = class_ids[index]
+        T_cad2cam_true = Ts_cad2cam_true[index]
+        cad_file = models.get_cad_model(class_id=class_id)
+        diagonal = models.get_bbox_diagonal(cad_file)
+        pitch = diagonal / dim
+        mask = instance_label == instance_id
+        extents = np.array((diagonal,) * 3)
+        grid_target, aabb_min, _ = get_instance_grid(
+            octrees,
+            pitch,
+            pcd,
+            mask,
+            instance_id,
+            extents,
+            threshold=connectivity * 0.75
+        )
+        dimension = np.array(grid_target.shape)
+        #
+        pcd_file = models.get_pcd_model(class_id=class_id)
+        points_source = np.loadtxt(pcd_file, dtype=np.float32)
+        points_source = contrib.extra.open3d.voxel_down_sample(
+            points_source, voxel_size=pitch
+        )
+        points_source = points_source.astype(np.float32)
+        #
+        centroid = aabb_min + pitch * dimension / 2
+        T_com2cam = tf.translation_matrix(centroid)
+        origin = - pitch * dimension / 2
+
+        registration = InstanceOccupancyGridRegistration(
+            points_source,
+            grid_target,
+            id_target=instance_id,
+            pitch=pitch,
+            origin=origin,
+            connectivity=connectivity,
+        )
+        Ts_cad2com_pred = registration.nstep(step)
+        Ts_cad2cam_pred = (T_com2cam @ T for T in Ts_cad2com_pred)
+
+        return registration, Ts_cad2cam_pred, T_cad2cam_true, T_com2cam
+
+
 def main():
     models = objslampp.datasets.YCBVideoModels()
     dataset = objslampp.datasets.YCBVideoDataset('train')
-    frame = dataset[1000]
-    class_ids = frame['meta']['cls_indexes']
-    instance_ids = class_ids
-    depth = frame['depth']
+    frame = dataset.get_example(1000)
+
+    # scene-level data
+    instance_ids = class_ids = frame['meta']['cls_indexes']
+    Ts_cad2cam_true = np.tile(np.eye(4), (len(instance_ids), 1, 1))
+    Ts_cad2cam_true[:, :3, :4] = frame['meta']['poses'].transpose(2, 0, 1)
     K = frame['meta']['intrinsic_matrix']
     pcd = objslampp.geometry.pointcloud_from_depth(
-        depth, fx=K[0, 0], fy=K[1, 1], cx=K[0, 2], cy=K[1, 2]
+        frame['depth'], fx=K[0, 0], fy=K[1, 1], cx=K[0, 2], cy=K[1, 2]
     )
     instance_label = frame['label']
 
-    index = 1
-    instance_id = instance_ids[index]
+    # instance-level data
+    instance_id = instance_ids[0]
+    index = np.where(instance_ids == instance_id)[0][0]
     class_id = class_ids[index]
-    T_cad2cam = np.r_[frame['meta']['poses'][:, :, index], [[0, 0, 0, 1]]]
-    pitch = models.get_bbox_diagonal(
-        models.get_cad_model(class_id=class_id)) / 20
-
-    nonnan = ~np.isnan(depth)
-    octrees = {}
-    for ins_id in np.r_[0, instance_ids]:
-        mask = instance_label == ins_id
-        octree = octomap.OcTree(pitch)
-        octree.insertPointCloud(
-            pcd[mask & nonnan],
-            np.array([0, 0, 0], dtype=float),
-        )
-        octrees[ins_id] = octree
-
-    mask = instance_label == instance_id
-    cad_file = models.get_cad_model(class_id=class_id)
-    diagonal = models.get_bbox_diagonal(cad_file)
-    extents = np.array((diagonal * 1.1,) * 3)
-    grid_target, origin, _ = get_instance_grid(
-        octrees, pitch, pcd, mask, instance_id, extents
-    )
-    dim = np.array(grid_target.shape)
-    print(dim)
-
-    pcd_file = models.get_pcd_model(class_id=class_id)
-    points = np.loadtxt(pcd_file, dtype=np.float32)
-
-    np.random.seed(2)
-    indices = np.random.permutation(len(points))[:1000]
-    points_source = points[indices]
-
-    transform_to_center = tf.translation_matrix(
-        - (origin + pitch * dim / 2))
-    origin = - pitch * dim / 2
-
-    T_cad2cam_true = transform_to_center @ T_cad2cam
-
     cad_file = models.get_cad_model(class_id=class_id)
     cad = trimesh.load(str(cad_file))
     cad.visual = cad.visual.to_color()
 
-    registration_occ = RegisterationOccupancyGrid(
-        points_source,
-        grid_target,
-        id_target=instance_id,
-        pitch=pitch,
-        origin=origin,
-        connectivity=2,
-    )
-    Ts_cad2cam_pred = registration_occ.nstep(100)
+    registration, Ts_cad2cam_pred, T_cad2cam_true, T_com2cam = \
+        SceneOccupancyGridRegistration(
+            instance_ids=instance_ids,
+            class_ids=class_ids,
+            Ts_cad2cam_true=Ts_cad2cam_true,
+            pcd=pcd,
+            instance_label=instance_label,
+        )(instance_id)
 
     # -------------------------------------------------------------------------
 
@@ -296,10 +379,11 @@ def main():
         except StopIteration:
             pyglet.clock.unschedule(callback)
             return
-        scenes = registration_occ.visualize(
+        scenes = registration.visualize(
             cad=cad,
-            transform_true=T_cad2cam_true,
-            transform_pred=T_cad2cam_pred,
+            T_cad2cam_true=T_cad2cam_true,
+            T_cad2cam_pred=T_cad2cam_pred,
+            T_com2cam=T_com2cam,
         )
         if widgets:
             for key, widget in widgets.items():
@@ -316,7 +400,10 @@ def main():
     scenes = callback(-1)
     for key, scene in scenes.items():
         widgets[key] = trimesh.viewer.SceneWidget(scene)
-        hbox.add(widgets[key])
+        vbox = glooey.VBox()
+        vbox.add(glooey.Label(key, color=(255, 255, 255)), size=0)
+        vbox.add(widgets[key])
+        hbox.add(vbox)
     gui.add(hbox)
 
     pyglet.clock.schedule_interval(callback, 1 / 30, widgets)
