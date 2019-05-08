37c413c1ed58122010bfd74c2dd9eeca9aee744f
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-05-08 14:50:08 +0100

    bg_class=True for occupancy.py

--- examples/ycb_video/singleview_3d/occupancy.py
+++ examples/ycb_video/singleview_3d/occupancy.py
@@ -12,14 +12,19 @@ from align_occupancy_grids import refinement
 
 models = objslampp.datasets.YCBVideoModels()
 inference = Inference(gpu=0)
-frame, Ts_cad2cam_true, Ts_cad2cam_pred = inference(index=0)
+frame, Ts_cad2cam_true, Ts_cad2cam_pred = inference(index=0, bg_class=True)
+
+keep = frame['class_ids'] > 0
+class_ids_fg = frame['class_ids'][keep]
+instance_ids_fg = frame['instance_ids'][keep]
+Ts_cad2cam_true_fg = frame['Ts_cad2cam'][keep]
 
 refinement(
-    instance_ids=frame['instance_ids'],
-    class_ids=frame['class_ids'],
+    instance_ids=instance_ids_fg,
+    class_ids=class_ids_fg,
     rgb=frame['rgb'],
     pcd=frame['pcd'],
     instance_label=frame['instance_label'],
-    Ts_cad2cam_true=frame['Ts_cad2cam'],
+    Ts_cad2cam_true=Ts_cad2cam_true,
     Ts_cad2cam_pred=Ts_cad2cam_pred,
 )
