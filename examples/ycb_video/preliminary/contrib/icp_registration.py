0a816449950afa204c3090f5f6a3400c5a7518f0
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-06-20 17:16:08 +0100

    Make iterative as an option

--- examples/ycb_video/preliminary/contrib/icp_registration.py
+++ examples/ycb_video/preliminary/contrib/icp_registration.py
@@ -15,11 +15,7 @@ class ICPRegistration:
             transform_init = np.eye(4)
         self._transform = transform_init
 
-    def register(
-        self,
-        iteration=100,
-        voxel_size=0.01,
-    ):
+    def _prepare(self, voxel_size):
         source = open3d.PointCloud()
         source.points = open3d.Vector3dVector(self._pcd_depth)
 
@@ -29,8 +25,30 @@ class ICPRegistration:
         source = open3d.voxel_down_sample(source, voxel_size=voxel_size)
         target = open3d.voxel_down_sample(target, voxel_size=voxel_size)
 
+        return source, target
+
+    def register(self, iteration=None, voxel_size=None):
+        iteration = 100 if iteration is None else iteration
+        voxel_size = 0.01 if voxel_size is None else voxel_size
+
+        source, target = self._prepare(voxel_size=voxel_size)
+        result = open3d.registration_icp(
+            source,  # points_from_depth
+            target,  # points_from_cad
+            2 * voxel_size,
+            tf.inverse_matrix(self._transform),
+            open3d.TransformationEstimationPointToPoint(False),
+            open3d.ICPConvergenceCriteria(max_iteration=iteration),
+        )
+        return tf.inverse_matrix(result.transformation)
+
+    def register_iterative(self, iteration=None, voxel_size=None):
+        iteration = 100 if iteration is None else iteration
+        voxel_size = 0.01 if voxel_size is None else voxel_size
+
         yield self._transform
 
+        source, target = self._prepare(voxel_size=voxel_size)
         for i in range(iteration):
             result = open3d.registration_icp(
                 source,  # points_from_depth
@@ -40,9 +58,8 @@ class ICPRegistration:
                 open3d.TransformationEstimationPointToPoint(False),
                 open3d.ICPConvergenceCriteria(max_iteration=1),
             )
-            if chainer.is_debug():
-                print(f'[{i:08d}] fitness={result.fitness:.2g} '
-                      f'inlier_rmse={result.inlier_rmse:.2g}')
+            print(f'[{i:08d}] fitness={result.fitness:.2g} '
+                  f'inlier_rmse={result.inlier_rmse:.2g}')
             self._transform = tf.inverse_matrix(
                 result.transformation
             )
