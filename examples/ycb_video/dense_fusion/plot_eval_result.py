5c045e456925996627728e062e03cd09fb07a822
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-06-20 19:50:32 +0100

    Add get_adds

--- examples/ycb_video/dense_fusion/plot_eval_result.py
+++ examples/ycb_video/dense_fusion/plot_eval_result.py
@@ -5,53 +5,12 @@ import collections
 import concurrent.futures
 
 import imgviz
-import numpy as np
 import matplotlib.pyplot as plt
 import path
-import scipy.io
-import trimesh.transformations as tf
 
 import objslampp
 
 import contrib
-
-
-def get_adds(result_file):
-    dataset = objslampp.datasets.YCBVideoDataset
-    models = objslampp.datasets.YCBVideoModels()
-
-    result = scipy.io.loadmat(
-        result_file, chars_as_strings=True, squeeze_me=True
-    )
-    frame_id = '/'.join(result['frame_id'].split('/')[1:])
-
-    frame = dataset.get_frame(frame_id)
-
-    adds = collections.defaultdict(list)
-    for gt_index, cls_id in enumerate(frame['meta']['cls_indexes']):
-        try:
-            pred_index = np.where(result['labels'] == cls_id)[0][0]
-            pose = result['poses'][pred_index]
-            T_pred = objslampp.geometry.compose_transform(
-                R=tf.quaternion_matrix(pose[:4])[:3, :3],
-                t=pose[4:],
-            )
-            T_true = np.r_[
-                frame['meta']['poses'][:, :, gt_index],
-                [[0, 0, 0, 1]],
-            ]
-            pcd_file = models.get_pcd_model(class_id=cls_id)
-            pcd = np.loadtxt(pcd_file)
-            add = objslampp.metrics.average_distance(
-                [pcd],
-                transform1=[T_true],
-                transform2=[T_pred],
-            )[0]
-        except IndexError:
-            add = np.inf
-        print(f'{result_file}, {cls_id:04d}, {add:.3f}')
-        adds[cls_id].append(add)
-    return adds
 
 
 here = path.Path(__file__).abspath().parent
@@ -73,7 +32,7 @@ def main():
     adds_list = []
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for result_file in result_dir.glob('*.mat'):
-            adds = executor.submit(get_adds, result_file)
+            adds = executor.submit(contrib.get_adds, result_file)
             adds_list.append(adds)
 
     adds_all = collections.defaultdict(list)
