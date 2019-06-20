ea1f2f42105f0c60146b672362552ed19326df9c
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-06-20 21:48:52 +0100

    Add OccupancyGridRegistration

--- examples/ycb_video/preliminary/contrib/__init__.py
+++ examples/ycb_video/preliminary/contrib/__init__.py
@@ -5,3 +5,5 @@ from .display_scenes import display_scenes
 from .icp_registration import ICPRegistration
 
 from .multi_instance_octree_mapping import MultiInstanceOctreeMapping
+
+from .occupancy_registration import OccupancyRegistration
