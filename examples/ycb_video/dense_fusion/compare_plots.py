336bfa188af342f15acc9d4a2e2bb45175ba6866
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-06-21 17:06:25 +0100

    Update examples/ycb_video/dense_fusion/compare_plots.py

--- examples/ycb_video/dense_fusion/compare_plots.py
+++ examples/ycb_video/dense_fusion/compare_plots.py
@@ -15,6 +15,7 @@ symmetric_objects = [
     '052_extra_large_clamp',
     '061_foam_brick',
 ]
+use_add_s = 1
 
 dfs = []
 for name in contrib.EVAL_RESULTS:
@@ -22,16 +23,25 @@ for name in contrib.EVAL_RESULTS:
     if not csv_file.exists():
         continue
     df = pandas.read_csv(csv_file, index_col=0)
-    # df = df[~df['class_name'].isin(symmetric_objects)]
-    df = df[['class_id', 'class_name', 'add_s_auc']]
+    df = df[~df['class_name'].isin(symmetric_objects)]
+    if use_add_s:
+        df = df[['class_id', 'class_name', 'add_s_auc']]
+    else:
+        df = df[['class_id', 'class_name', 'add_auc']]
     df = df.set_index(['class_id', 'class_name'])
-    df = df.rename(columns={'add_s_auc': f'{name} (ADD-S)'})
+    if use_add_s:
+        df = df.rename(columns={'add_s_auc': f'{name} (ADD-S)'})
+    else:
+        df = df.rename(columns={'add_auc': f'{name} (ADD)'})
     dfs.append(df)
 df = pandas.concat(dfs, axis=1, sort=False)
 df = df.sort_index()
 # print(df)
 
-baseline_name = 'Densefusion_wo_refine_result (ADD-S)'
+if use_add_s:
+    baseline_name = 'Densefusion_wo_refine_result (ADD-S)'
+else:
+    baseline_name = 'Densefusion_wo_refine_result (ADD)'
 baseline = df[baseline_name]
 for col in df.columns:
     if col != baseline_name:
