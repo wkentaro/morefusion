5f3641eb156266aae1ca7d5b1acd42c46135c448
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-07-02 11:34:10 +0100

    Fix get_target_pcds and get_target_grids

--- examples/ycb_video/preliminary/preliminary/multi_instance_octree_mapping.py
+++ examples/ycb_video/preliminary/preliminary/multi_instance_octree_mapping.py
@@ -1,6 +1,5 @@
 import numpy as np
 import octomap
-import sklearn.neighbors
 import trimesh
 
 
@@ -35,6 +34,28 @@ class MultiInstanceOctreeMapping:
             self._pcds.pop(instance_id)  # clear cache
 
     def get_target_grids(self, target_id, *, dimensions, pitch, origin):
+        '''Get voxel grids of the specified instance.
+
+        Parameters
+        ----------
+        target_id: int
+            Target instance ID.
+        dimensions: (3,) array-like, int
+            Voxel dimensions (e.g., 32x32x32).
+        pitch: float
+            Voxel pitch.
+        origin: (3,) array-like, float
+            Voxel origin.
+
+        Returns
+        -------
+        grid_target: numpy.ndarray
+            Occupied space of the target instance.
+        grid_nontarget: numpy.ndarray
+            Occupied space of non-target instance.
+        grid_empty: numpy.ndarray
+            Empty space.
+        '''
         grid_target = np.zeros(dimensions, dtype=np.float32)
         grid_nontarget = np.zeros(dimensions, dtype=np.float32)
         grid_empty = np.zeros(dimensions, np.float32)
@@ -44,38 +65,51 @@ class MultiInstanceOctreeMapping:
         )
 
         for ins_id, octree in self._octrees.items():
-            occupied, empty = octree.extractPointCloud()
-
-            if occupied.size:
-                kdtree = sklearn.neighbors.KDTree(occupied)
-                dist, indices = kdtree.query(centers, k=1)
-
-                if ins_id == target_id:
-                    # occupied by target
-                    g = np.minimum(1, np.maximum(0, 1 - (dist / pitch)))
-                    g = g.reshape(dimensions)
-                    grid_target = np.maximum(grid_target, g)
+            for center in centers:
+                node = octree.search(center)
+                try:
+                    occupancy = node.getValue()
+                except octomap.NullPointerException:
+                    continue
+                i, j, k = trimesh.voxel.points_to_indices(
+                    [center], pitch=pitch, origin=origin)[0]
+                if occupancy > 0.5:
+                    if ins_id == target_id:
+                        grid_target[i, j, k] = occupancy
+                    else:
+                        grid_nontarget[i, j, k] = occupancy
                 else:
-                    # occupied by non-target
-                    g = np.minimum(1, np.maximum(0, 1 - (dist / pitch)))
-                    g = g.reshape(dimensions)
-                    grid_nontarget = np.maximum(grid_nontarget, g)
-
-            if empty.size:
-                # empty
-                kdtree = sklearn.neighbors.KDTree(empty)
-                dist, indices = kdtree.query(centers, k=1)
-                g = np.minimum(1, np.maximum(0, 1 - (dist / pitch)))
-                g = g.reshape(dimensions)
-                grid_empty = np.maximum(grid_empty, g)
+                    grid_empty[i, j, k] = 1 - occupancy
 
         return grid_target, grid_nontarget, grid_empty
 
     def get_target_pcds(self, target_id, aabb_min=None, aabb_max=None):
+        '''Get point clouds of the specified instance.
+
+        Parameters
+        ----------
+        target_id: int
+            Target instance ID.
+        aabb_min: (3,) array-like, optional
+            AABB minimum xyz.
+        aabb_max: (3,) array-like, optional
+            AABB maximum xyz.
+
+        Returns
+        -------
+        occupied: (N, 3) numpy.ndarray, np.float64
+            Occupied points.
+        empty: (M, 3) numpy.ndarray, np.float64
+            Empty points.
+        '''
         octree = self._octrees[target_id]
         if target_id not in self._pcds:
-            occupied, empty = octree.extractPointCloud(
-                bbx_min=aabb_min, bbx_max=aabb_max
-            )
+            occupied, empty = octree.extractPointCloud()
+            if aabb_min is not None:
+                occupied = occupied[(occupied >= aabb_min).all(axis=1)]
+                empty = empty[(empty >= aabb_min).all(axis=1)]
+            if aabb_max is not None:
+                occupied = occupied[(occupied < aabb_max).all(axis=1)]
+                empty = empty[(empty < aabb_max).all(axis=1)]
             self._pcds[target_id] = (occupied, empty)
         return self._pcds[target_id]
