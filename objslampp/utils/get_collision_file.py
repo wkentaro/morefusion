3cfabce7b416fa32a24a4eae0669f5d512ff8ff7
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-07-03 15:04:20 +0100

    _convex -> .convex

--- objslampp/utils/get_collision_file.py
+++ objslampp/utils/get_collision_file.py
@@ -6,7 +6,7 @@ import path
 
 def get_collision_file(visual_file):
     visual_file = path.Path(visual_file)
-    collision_file = visual_file.stripext() + '_convex' + visual_file.ext
+    collision_file = visual_file.stripext() + '.convex' + visual_file.ext
     if not collision_file.exists():
         cmd = f'testVHACD --input {visual_file} --output {collision_file}'\
               ' --log /tmp/testVHACD.log --resolution 200000'
