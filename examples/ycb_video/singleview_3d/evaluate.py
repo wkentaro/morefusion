1e0d0929c042b80c0475c97e75b1073adc82eef3
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-07-05 14:35:21 +0100

    Fix evaluate.py for multiple classes

--- examples/ycb_video/singleview_3d/evaluate.py
+++ examples/ycb_video/singleview_3d/evaluate.py
@@ -8,11 +8,10 @@ import chainer
 from chainer.backends import cuda
 import imgviz
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas
 import path
 import pybullet  # NOQA
-import trimesh
-import trimesh.transformations as tf
 
 import objslampp
 
@@ -25,8 +24,6 @@ def main():
     )
     parser.add_argument('model', help='model file in a log dir')
     parser.add_argument('--gpu', type=int, default=0, help='gpu id')
-    parser.add_argument('--show', action='store_true', help='show')
-    parser.add_argument('--save', action='store_true', help='save')
     parser.add_argument(
         '--dataset',
         default='ycb_video',
@@ -45,8 +42,10 @@ def main():
         chainer.cuda.get_device_from_id(args.gpu).use()
 
     model = contrib.models.BaselineModel(
+        n_fg_class=len(args_data['class_names'][1:]),
         freeze_until=args_data['freeze_until'],
         voxelization=args_data.get('voxelization', 'average'),
+        use_occupancy=args_data.get('use_occupancy', False),
     )
     if args.gpu >= 0:
         model.to_gpu()
@@ -56,7 +55,11 @@ def main():
     print('==> Done model loading')
 
     if args.dataset == 'ycb_video':
-        dataset = contrib.datasets.YCBVideoDataset('val', class_ids=[2])
+        dataset = contrib.datasets.YCBVideoDataset(
+            'val',
+            class_ids=args_data['class_ids'],
+            return_occupancy_grids=args_data['use_occupancy'],
+        )
     else:
         assert args.dataset == 'bin_type'
         dataset = contrib.datasets.MySyntheticDataset(
@@ -65,25 +68,53 @@ def main():
         )
         dataset.instance_id = args.instance_id
 
+    def transform(in_data):
+        if args_data.get('use_occupancy', False):
+            assert 'grid_target' in in_data
+            assert 'grid_nontarget' in in_data
+            assert 'grid_empty' in in_data
+
+            grid_nontarget_empty = np.maximum(
+                in_data['grid_nontarget'], in_data['grid_empty']
+            )
+            grid_nontarget_empty = np.float64(grid_nontarget_empty > 0.5)
+            grid_nontarget_empty[in_data['grid_target'] > 0.5] = 0
+            in_data['grid_nontarget_empty'] = grid_nontarget_empty
+            in_data.pop('grid_target')
+            in_data.pop('grid_nontarget')
+            in_data.pop('grid_empty')
+        return in_data
+
     # -------------------------------------------------------------------------
 
     observations = []
-    depth2rgb = imgviz.Depth2RGB()
     for index in range(len(dataset)):
-        with chainer.using_config('debug', True):
-            examples = dataset[index:index + 1]
+        examples = dataset[index:index + 1]
+        examples = [
+            transform(e) for e in examples
+            if e['class_id'] in dataset._class_ids
+        ]
+        if not examples:
+            continue
         inputs = chainer.dataset.concat_examples(examples, device=args.gpu)
 
-        if inputs['class_id'][0] == -1:
-            continue
+        keep = inputs['class_id'] != -1
+        for key in inputs.keys():
+            inputs[key] = inputs[key][keep]
+        # make sure there are no multiple instances
+        assert np.unique(
+            cuda.to_cpu(inputs['class_id']), return_counts=True
+        )[1].max() == 1
 
         with chainer.no_backprop_mode() and \
                 chainer.using_config('train', False):
             quaternion_pred, translation_pred = model.predict(
                 class_id=inputs['class_id'],
                 pitch=inputs['pitch'],
+                origin=inputs['origin'],
                 rgb=inputs['rgb'],
                 pcd=inputs['pcd'],
+                grid_nontarget_empty=inputs.get('grid_nontarget_empty'),
             )
 
             reporter = chainer.Reporter()
@@ -101,112 +132,60 @@ def main():
 
         print(f'[{index:08d}] {observation}')
 
-        if not (args.show or args.save):
+    df = pandas.DataFrame(observations)
+    out_file = path.Path(args.model).parent / 'add.csv'
+    df.to_csv(out_file)
+    print('==> Saved ADD CSV:', out_file)
+
+    aucs = {}
+    for class_id, class_name in enumerate(args_data['class_names']):
+        if class_id == 0:
+            continue
+        if args_data['class_ids'] and class_id not in args_data['class_ids']:
             continue
 
-        # ---------------------------------------------------------------------
+        try:
+            add = df[f'main/add/{class_id:04d}'].dropna().values
+        except KeyError:
+            continue
+        auc, x, y = objslampp.metrics.ycb_video_add_auc(add, return_xy=True)
+        print('AUC(ADD):', auc)
 
-        class_id = int(inputs['class_id'][0])
+        add = df[f'main/add_s/{class_id:04d}'].dropna().values
+        auc_s, x, y = objslampp.metrics.ycb_video_add_auc(add, return_xy=True)
+        print('AUC (ADD-S):', auc_s)
 
-        frame = dataset.get_frame(index)
-        rgb = frame['rgb']
-        K = frame['intrinsic_matrix']
-        height, width = rgb.shape[:2]
-        fovy = trimesh.scene.Camera(
-            resolution=(width, height), focal=(K[0, 0], K[1, 1])
-        ).fov[1]
-        cad_file = objslampp.datasets.YCBVideoModels()\
-            .get_cad_file(class_id=class_id)
+        aucs[class_id] = {'add': auc, 'add_s': auc_s}
 
-        quaternion_pred = cuda.to_cpu(quaternion_pred.array)[0]
-        translation_pred = cuda.to_cpu(translation_pred.array)[0]
-        T_pred = tf.quaternion_matrix(quaternion_pred)
-        T_pred[:3, 3] = translation_pred
-        quaternion_true = cuda.to_cpu(inputs['quaternion_true'])[0]
-        translation_true = cuda.to_cpu(inputs['translation_true'])[0]
-        T_true = tf.quaternion_matrix(quaternion_true)
-        T_true[:3, 3] = translation_true
+        fig = plt.figure(figsize=(10, 5))
 
-        Ts = {'true': T_true, 'pred': T_pred}
+        plt.subplot(121)
+        plt.title('ADD (AUC={:.1f})'.format(auc * 100))
+        plt.plot(x, y, color='b')
+        plt.xlim(0, 0.1)
+        plt.ylim(0, 1)
+        plt.xlabel('average distance threshold [m]')
+        plt.ylabel('accuracy')
 
-        vizs = []
-        for which in ['true', 'pred']:
-            rgb_rend, depth_rend, mask_rend = \
-                objslampp.extra.pybullet.render_cad(
-                    cad_file, Ts[which], fovy=fovy, height=height, width=width
-                )
-            mask_rend = imgviz.label2rgb(mask_rend * 2, img=rgb, alpha=0.7)
-            depth_rend = depth2rgb(depth_rend)
-            viz = imgviz.tile(
-                [rgb, examples[0]['rgb'], mask_rend, rgb_rend, depth_rend],
-                (1, 5),
-                border=(255, 255, 255),
-            )
-            viz = imgviz.resize(viz, width=1800)
+        plt.subplot(122)
+        plt.title('ADD (AUC={:.1f})'.format(auc_s * 100))
+        plt.plot(x, y, color='b')
+        plt.xlim(0, 0.1)
+        plt.ylim(0, 1)
+        plt.xlabel('average distance threshold [m]')
+        plt.ylabel('accuracy')
 
-            add = observation[f'main/add/{class_id:04d}']
-            add_rot = observation[f'main/add_rotation/{class_id:04d}']
-            text = f'[{which}]: add={add * 100:.1f}cm, add_rot={add_rot * 100:.1f}cm' # NOQA
-            viz = imgviz.draw.text_in_rectangle(
-                viz, loc='lt', text=text, size=20, background=(0, 255, 0),
-            )
-            if which == 'true':
-                viz = imgviz.draw.text_in_rectangle(
-                    viz,
-                    loc='rt',
-                    text='singleview_3d',
-                    size=20,
-                    background=(255, 0, 0),
-                )
-            vizs.append(viz)
-        viz = imgviz.tile(vizs, (2, 1), border=(255, 255, 255))
+        plt.tight_layout()
+        img = imgviz.io.pyplot_fig2arr(fig)
+        out_file = path.Path(args.model).parent / f'auc_{class_id:04d}.png'
+        print('==> Saved ADD curve plot:', out_file)
+        imgviz.io.imsave(out_file, img)
 
-        if args.save:
-            out_file = path.Path(args.model).parent / f'video/{index:08d}.jpg'
-            out_file.parent.makedirs_p()
-            imgviz.io.imsave(out_file, viz)
-
-        if args.show:
-            yield viz
-
-    df = pandas.DataFrame(observations)
-
-    errors = df['main/add_rotation/0002'].values
-    auc, x, y = objslampp.metrics.auc_for_errors(errors, 0.1, return_xy=True)
-    print('auc (add_rotation):', auc)
-
-    fig = plt.figure(figsize=(10, 5))
-
-    plt.subplot(121)
-    plt.title('ADD (rotation) (AUC={:.1f})'.format(auc * 100))
-    plt.plot(x, y, color='b')
-    plt.xlim(0, 0.1)
-    plt.ylim(0, 1)
-    plt.xlabel('average distance threshold [m]')
-    plt.ylabel('accuracy')
-
-    plt.subplot(122)
-    errors = df['main/add/0002'].values
-    auc, x, y = objslampp.metrics.auc_for_errors(errors, 0.1, return_xy=True)
-    print('auc (add):', auc)
-    plt.title('ADD (rotation + translation) (AUC={:.1f})'.format(auc * 100))
-    plt.plot(x, y, color='b')
-    plt.xlim(0, 0.1)
-    plt.ylim(0, 1)
-    plt.xlabel('average distance threshold [m]')
-    plt.ylabel('accuracy')
-
-    plt.tight_layout()
-    img = imgviz.io.pyplot_fig2arr(fig)
-    out_file = path.Path(args.model).parent / 'auc.png'
-    print('==> Saved ADD curve plot:', out_file)
-    imgviz.io.imsave(out_file, img)
+    df = pandas.DataFrame(aucs)
+    out_file = path.Path(args.model).parent / 'auc.csv'
+    df.to_csv(out_file)
+    print('==> Saved AUC CSV:', out_file)
 
 
 if __name__ == '__main__':
-    vizs = main()
-    try:
-        imgviz.io.pyglet_imshow(vizs)
-        imgviz.io.pyglet_run()
-    except StopIteration:
-        pass
+    main()
