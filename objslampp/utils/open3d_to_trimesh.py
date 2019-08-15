9902de6dddd1ed11e79001ae876a75682a4fa270
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-08-15 12:04:19 +0100

    Fix open3d_to_trimesh

--- objslampp/utils/open3d_to_trimesh.py
+++ objslampp/utils/open3d_to_trimesh.py
@@ -9,8 +9,9 @@ def open3d_to_trimesh(src):
             vertices=np.asarray(src.vertices),
             faces=np.asarray(src.triangles),
             vertex_normals=np.asarray(src.vertex_normals),
+            vertex_colors=np.asarray(src.vertex_colors)
+                if src.has_vertex_colors else None,
         )
-        dst.visual.vertex_colors = np.asarray(src.vertex_colors)
     else:
         raise ValueError('Unsupported type of src: {}'.format(type(src)))
 
