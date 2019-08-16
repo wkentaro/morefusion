40bb680491f30e2720f137a433d0cc93a6668ed8
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-08-16 12:49:16 +0100

    Fix for flake8

--- objslampp/utils/open3d_to_trimesh.py
+++ objslampp/utils/open3d_to_trimesh.py
@@ -5,12 +5,14 @@ import trimesh
 
 def open3d_to_trimesh(src):
     if isinstance(src, open3d.TriangleMesh):
+        vertex_colors = None
+        if src.has_vertex_colors:
+            vertex_colors = np.asarray(src.vertex_colors)
         dst = trimesh.Trimesh(
             vertices=np.asarray(src.vertices),
             faces=np.asarray(src.triangles),
             vertex_normals=np.asarray(src.vertex_normals),
-            vertex_colors=np.asarray(src.vertex_colors)
-                if src.has_vertex_colors else None,
+            vertex_colors=vertex_colors,
         )
     else:
         raise ValueError('Unsupported type of src: {}'.format(type(src)))
