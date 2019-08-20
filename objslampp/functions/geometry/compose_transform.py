f3f35add47451b1ef2c5fbdf77946bff9ffc4356
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-08-20 10:57:00 +0100

    Ts -> T, Rs -> R, ts -> t in compose_transform

--- objslampp/functions/geometry/compose_transform.py
+++ objslampp/functions/geometry/compose_transform.py
@@ -7,32 +7,32 @@ class ComposeTransform(chainer.Function):
     def check_type_forward(self, in_types):
         chainer.utils.type_check.expect(in_types.size() == 2)
 
-        Rs_type, ts_type = in_types
+        R_type, t_type = in_types
         chainer.utils.type_check.expect(
-            Rs_type.ndim == 3,  # N, 3, 3
-            Rs_type.shape[0] == ts_type.shape[0],
-            Rs_type.shape[1:3] == (3, 3),
-            ts_type.ndim == 2,  # N, 3
-            ts_type.shape[1] == 3,
+            R_type.ndim == 3,  # N, 3, 3
+            R_type.shape[0] == t_type.shape[0],
+            R_type.shape[1:3] == (3, 3),
+            t_type.ndim == 2,  # N, 3
+            t_type.shape[1] == 3,
         )
 
     def forward(self, inputs):
         xp = cuda.get_array_module(*inputs)
-        Rs, ts = inputs
+        R, t = inputs
 
-        batch_size = Rs.shape[0]
-        Ts = xp.eye(4, dtype=Rs.dtype)
-        Ts = Ts[None].repeat(batch_size, axis=0)
+        batch_size = R.shape[0]
+        T = xp.eye(4, dtype=R.dtype)
+        T = T[None].repeat(batch_size, axis=0)
 
-        Ts[:, :3, :3] = Rs
-        Ts[:, :3, 3] = ts
-        return Ts,
+        T[:, :3, :3] = R
+        T[:, :3, 3] = t
+        return T,
 
     def backward(self, inputs, gy):
-        gTs, = gy
-        gRs = gTs[:, :3, :3]
-        gts = gTs[:, :3, 3]
-        return gRs, gts
+        gT, = gy
+        gR = gT[:, :3, :3]
+        gt = gT[:, :3, 3]
+        return gR, gt
 
 
 def compose_transform(Rs, ts):
