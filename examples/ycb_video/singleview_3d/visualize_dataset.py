1d68f586932f4fa54e5662bed8adc0f7a45d4ebc
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-08-27 11:56:13 +0100

    Add --randomize-base to visualize_dataset.py

--- examples/ycb_video/singleview_3d/visualize_dataset.py
+++ examples/ycb_video/singleview_3d/visualize_dataset.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
+import argparse
+
 import numpy as np
 import trimesh
+import trimesh.transformations as tf
 
 import objslampp
 
 import contrib
 
 
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument('--randomize-base', action='store_true')
+args = parser.parse_args()
+
+models = objslampp.datasets.YCBVideoModels()
 dataset = contrib.datasets.YCBVideoDataset('train')
 
 frame = dataset.get_frame(0)
@@ -15,20 +25,37 @@ examples = dataset.get_example(0)
 
 scene = trimesh.Scene()
 for example in examples:
-    example['class_id']
-    example['origin']
-    example['pitch']
+    mask = ~np.isnan(example['pcd']).any(axis=2)
+    points = example['pcd'][mask]
+    values = example['rgb'][mask]
+
+    T_cad2cam = objslampp.functions.transformation_matrix(
+        example['quaternion_true'], example['translation_true']
+    ).array
+    if args.randomize_base:
+        points = tf.transform_points(points, np.linalg.inv(T_cad2cam))
+        T_rand = tf.random_rotation_matrix()
+        T_cad2cam = T_cad2cam @ T_rand
+        points = tf.transform_points(points, T_cad2cam)
+
+    pitch = models.get_voxel_pitch(32, example['class_id'])
+    center = np.median(points, axis=0)
+    origin = center - pitch * 15.5
 
     mapping = objslampp.geometry.VoxelMapping(
-        origin=example['origin'],
-        pitch=example['pitch'],
+        origin=origin,
+        pitch=pitch,
         voxel_dim=32,
         nchannel=3,
     )
-    mask = ~np.isnan(example['pcd']).any(axis=2)
-    mapping.add(example['pcd'][mask], example['rgb'][mask])
+
+    mapping.add(points, values)
     scene.add_geometry(mapping.as_boxes())
     scene.add_geometry(mapping.as_bbox())
+    cad = models.get_cad(example['class_id'])
+    if hasattr(cad.visual, 'to_color'):
+        cad.visual = cad.visual.to_color()
+    scene.add_geometry(cad, transform=T_cad2cam)
 scene.camera.resolution = (640, 480)
 K = frame['intrinsic_matrix']
 scene.camera.focal = (K[0, 0], K[1, 1])
