49e88f2090be19d36af7d13accf85b642f0d63a2
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-09-02 01:53:20 +0900

    Update examples/ycb_video/singleview_pcd/train.py

--- examples/ycb_video/singleview_pcd/train.py
+++ examples/ycb_video/singleview_pcd/train.py
@@ -131,7 +131,7 @@ def main():
         )
 
         if data_valid is None:
-            data_valid = contrib.datasets.YCBVideoDataset(
+            data_valid = contrib.datasets.YCBVideoSingleInstanceDataset(
                 'val',
                 class_ids=args.class_ids,
             )
@@ -151,7 +151,7 @@ def main():
         model.to_gpu()
 
     # optimizer initialization
-    optimizer = chainer.optimizers.Adam(alpha=args.lr)
+    optimizer = chainer.optimizers.Adam(alpha=args.lr * n_gpu)
     if args.multi_node:
         optimizer = chainermn.create_multi_node_optimizer(optimizer, comm)
     optimizer.setup(model)
