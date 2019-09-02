be7e3b2f01785603eedbc19629acc7f371c1c75e
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-09-02 15:03:05 +0100

    Add chainer version of ICPRegistration

--- examples/ycb_video/preliminary/icp.py
+++ examples/ycb_video/preliminary/icp.py
@@ -5,6 +5,7 @@ from chainer.backends import cuda
 import chainer.functions as F
 import numpy as np
 import trimesh
+import trimesh.transformations as tf
 
 import objslampp
 
@@ -52,20 +53,75 @@ class NearestNeighborICP(chainer.Link):
         return loss
 
 
+class ICPRegistration:
+
+    def __init__(
+        self,
+        points_depth,
+        points_cad,
+        transform_init=None,
+        alpha=0.1,
+        gpu=0,
+    ):
+        quaternion_init = tf.quaternion_from_matrix(transform_init)
+        quaternion_init = quaternion_init.astype(np.float32)
+        translation_init = tf.translation_from_matrix(transform_init)
+        translation_init = translation_init.astype(np.float32)
+
+        link = NearestNeighborICP(quaternion_init, translation_init)
+
+        if gpu >= 0:
+            link.to_gpu(gpu)
+            points_depth = link.xp.asarray(points_depth)
+            points_cad = link.xp.asarray(points_cad)
+        self._points_depth = points_depth
+        self._points_cad = points_cad
+
+        self._optimizer = chainer.optimizers.Adam(alpha=alpha)
+        self._optimizer.setup(link)
+        link.translation.update_rule.hyperparam.alpha *= 0.1
+
+    @property
+    def _transform(self):
+        return cuda.to_cpu(self._optimizer.target.T.array)
+
+    def register_iterative(self, iteration=None):
+        iteration = 100 if iteration is None else iteration
+
+        yield self._transform
+
+        for i in range(iteration):
+            link = self._optimizer.target
+            loss = link(
+                source=self._points_cad,
+                target=self._points_depth,
+            )
+            loss.backward()
+            self._optimizer.update()
+            link.cleargrads()
+
+            # print(f'[{self._iteration:08d}] {loss}')
+            # print(f'quaternion:', link.quaternion.array.tolist())
+            # print(f'translation:', link.translation.array.tolist())
+
+            yield self._transform
+
+
 def algorithm():
     gpu = 0
     if gpu >= 0:
         cuda.get_device_from_id(gpu).use()
 
+    instance_id = 3  # == class_id
+
     models = objslampp.datasets.YCBVideoModels()
-    pcd_cad = models.get_pcd(class_id=2)
+    pcd_cad = models.get_pcd(class_id=instance_id)
 
     dataset = objslampp.datasets.YCBVideoDataset('train')
     example = dataset.get_example(1000)
 
     depth = example['depth']
     instance_label = example['label']
-    instance_id = 2
     K = example['meta']['intrinsic_matrix']
     pcd_depth = objslampp.geometry.pointcloud_from_depth(
         depth, fx=K[0, 0], fy=K[1, 1], cx=K[0, 2], cy=K[1, 2]
@@ -76,51 +132,30 @@ def algorithm():
 
     # -------------------------------------------------------------------------
 
+    quaternion_init = np.array([1, 0, 0, 0], dtype=np.float32)
+    translation_init = np.median(pcd_depth_target, axis=0)
+    transform_init = objslampp.functions.transformation_matrix(
+        quaternion_init, translation_init
+    ).array
+
     pcd_cad = objslampp.extra.open3d.voxel_down_sample(
         pcd_cad, voxel_size=0.01
     )
     pcd_depth_target = objslampp.extra.open3d.voxel_down_sample(
         pcd_depth_target, voxel_size=0.01
     )
+    registration = ICPRegistration(pcd_depth_target, pcd_cad, transform_init)
 
-    pcd_cad = pcd_cad.astype(np.float32)
-    pcd_depth_target = pcd_depth_target.astype(np.float32)
-    pcd_cad_cpu = pcd_cad
-    pcd_depth_target_cpu = pcd_depth_target
-    if gpu >= 0:
-        pcd_cad = cuda.to_gpu(pcd_cad)
-        pcd_depth_target = cuda.to_gpu(pcd_depth_target)
-
-    quaternion_init = np.array([1, 0, 0, 0], dtype=np.float32)
-    translation_init = pcd_depth_target.mean(axis=0)
-    nnicp = NearestNeighborICP(
-        quaternion_init=quaternion_init,
-        translation_init=translation_init,
-    )
-    if gpu >= 0:
-        nnicp.to_gpu()
-
-    optimizer = chainer.optimizers.Adam(alpha=0.1)
-    optimizer.setup(nnicp)
-    nnicp.translation.update_rule.hyperparam.alpha *= 0.1
-
-    for i in range(300):
-        T_cad2cam = cuda.to_cpu(nnicp.T.array)
-
+    for T_cad2cam in registration.register_iterative():
         scene = trimesh.Scene()
-        geom = trimesh.PointCloud(pcd_depth_target_cpu, colors=[1., 0, 0])
+        geom = trimesh.PointCloud(pcd_depth_target, colors=[1., 0, 0])
         scene.add_geometry(geom, geom_name='a', node_name='a')
-        geom = trimesh.PointCloud(pcd_cad_cpu, colors=[0, 1., 0])
+        geom = trimesh.PointCloud(pcd_cad, colors=[0, 1., 0])
         scene.add_geometry(
             geom, geom_name='b', node_name='b', transform=T_cad2cam
         )
         scene.camera.transform = objslampp.extra.trimesh.to_opengl_transform()
         yield scene
-
-        optimizer.target.cleargrads()
-        loss = optimizer.target(source=pcd_cad, target=pcd_depth_target)
-        loss.backward()
-        optimizer.update()
 
 
 def main():
