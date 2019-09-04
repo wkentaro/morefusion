75b12de4009ddbafa50067b8b27861a1b74151cb
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-09-04 14:59:34 +0100

    Remove show_logs.sh

--- examples/ycb_video/singleview_3d/show_logs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-logtable --filter class_names multi_node gpu max_epoch voxelization '.*main/add.*' '.*main/loss.*' freeze_until githash hostname debug 'validation/main/auc.*(min)' '.*add_r.*' 'validation/main/auc/add/.*' 'validation/main/auc/add_s/.*' nocall_evaluation_before_training timestamp out seed $*
