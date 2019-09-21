e5bb063388acb74bcf325c6871e67d7442ab0f62
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-09-21 16:55:33 +0100

    Add reindex func

---
+++ objslampp/datasets/rgbd_pose_estimation/reindex.py
@@ -0,0 +1,37 @@
+import json
+
+import concurrent.futures
+import numpy as np
+import tqdm
+
+
+def task(reindexed_root_dir, dataset, index):
+    id = dataset._ids[index]
+    examples = dataset.get_example(index)
+    id_to_class_id = {}
+    for ind, example in enumerate(examples):
+        id = f'{id}/{ind:08d}'
+        npz_file = reindexed_root_dir / f'{id}.npz'
+        npz_file.parent.makedirs_p()
+        np.savez_compressed(npz_file, **example)
+        id_to_class_id[id] = example['class_id']
+    return id_to_class_id
+
+
+def reindex(reindexed_root_dir: str, datasets: list):
+    id_to_class_id: dict = {}
+
+    executor = concurrent.futures.ProcessPoolExecutor()
+
+    for dataset in datasets:
+        futures = []
+        for index in range(len(dataset)):
+            future = executor.submit(task, reindexed_root_dir, dataset, index)
+            futures.append(future)
+
+        for index, future in tqdm.tqdm(enumerate(futures), total=len(futures)):
+            for id, class_id in future.result().items():
+                id_to_class_id[id] = int(class_id)
+
+    with open(reindexed_root_dir / 'id_to_class_id.json', 'w') as f:
+        json.dump(id_to_class_id, f, indent=4)
--- objslampp/datasets/rgbd_pose_estimation/ycb_video_posecnn_results/reindex.py
+++ objslampp/datasets/rgbd_pose_estimation/ycb_video_posecnn_results/reindex.py
@@ -1,45 +1,8 @@
-import json
-
-import concurrent.futures
-import numpy as np
-import tqdm
-
+from ..reindex import reindex
 from .dataset import YCBVideoPoseCNNResultsRGBDPoseEstimationDataset
 
 
-dataset = YCBVideoPoseCNNResultsRGBDPoseEstimationDataset()
-
-root_dir = dataset.root_dir + '.reindexed'
-
-
-def task(index):
-    image_id = dataset._ids[index]
-    examples = dataset.get_example(index)
-    id_to_class_id = {}
-    for ind, example in enumerate(examples):
-        id = f'data/{image_id}/{ind:08d}'
-        npz_file = root_dir / f'{id}.npz'
-        npz_file.parent.makedirs_p()
-        np.savez_compressed(npz_file, **example)
-        id_to_class_id[id] = example['class_id']
-    return id_to_class_id
-
-
-def main():
-    id_to_class_id = {}
-    executor = concurrent.futures.ProcessPoolExecutor()
-    futures = []
-    for index in range(len(dataset)):
-        future = executor.submit(task, index)
-        futures.append(future)
-
-    for index, future in tqdm.tqdm(enumerate(futures), total=len(futures)):
-        for id, class_id in future.result().items():
-            id_to_class_id[id] = int(class_id)
-
-    with open(root_dir / 'id_to_class_id.json', 'w') as f:
-        json.dump(id_to_class_id, f, indent=4)
-
-
 if __name__ == '__main__':
-    main()
+    dataset = YCBVideoPoseCNNResultsRGBDPoseEstimationDataset()
+    reindexed_root_dir = dataset.root_dir + '.reindexed'
+    reindex(reindexed_root_dir, [dataset])
