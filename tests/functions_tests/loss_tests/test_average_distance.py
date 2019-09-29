237036fe40dc0a9360436ce11625b5e79106ae6f
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-09-29 16:36:02 +0100

    Fix tests

--- tests/functions_tests/loss_tests/test_average_distance.py
+++ tests/functions_tests/loss_tests/test_average_distance.py
@@ -7,32 +7,29 @@ from chainer import testing
 import numpy as np
 
 from objslampp import metrics
-from objslampp.functions.loss.average_distance import average_distance_l1
-from objslampp.functions.loss.average_distance import average_distance_l2
+from objslampp.functions.loss.average_distance import average_distance
 
 
 class TestAverageDistance(unittest.TestCase):
 
     def setUp(self):
         self.points = tf.random_vector((128, 3)).astype(np.float32)
-        self.T1 = np.asarray([tf.random_rotation_matrix() for _ in range(5)])
-        self.T1[:, :3, 3] = tf.random_vector((5, 3))
+        self.T1 = tf.random_rotation_matrix()
+        self.T1[:3, 3] = tf.random_vector((3,))
         self.T1 = self.T1.astype(np.float32)
         self.T2 = np.asarray([tf.random_rotation_matrix() for _ in range(5)])
         self.T2[:, :3, 3] = tf.random_vector((5, 3))
         self.T2 = self.T2.astype(np.float32)
 
     def check_forward(self, points, T1, T2):
-        d1 = average_distance_l1(points, T1, T2)
+        d1 = average_distance(points, T1, T2)
         d1 = cuda.to_cpu(d1.array)
         d2 = metrics.average_distance(
-            [cuda.to_cpu(points)] * len(T1),
-            cuda.to_cpu(T1),
+            [cuda.to_cpu(points)] * len(T2),
+            [cuda.to_cpu(T1)] * len(T2),
             cuda.to_cpu(T2),
         )[0]
         testing.assert_allclose(d1, d2)
-
-        average_distance_l2(points, T1, T2)
 
     def test_forward_cpu(self):
         self.check_forward(self.points, self.T1, self.T2)
