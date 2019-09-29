d99cd0423b7d7841b2df3aa5a872ebfa15cd9a1d
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-09-29 19:14:00 +0100

    Stop using compressed version since its slow (~4 - 13 fps)

--- ros/ros_objslampp_ycb_video/nodes/rosbag_play_rs_rgbd.py
+++ ros/ros_objslampp_ycb_video/nodes/rosbag_play_rs_rgbd.py
@@ -8,8 +8,8 @@ import rospy
 
 
 bag_file = gdown.cached_download(
-    url='https://drive.google.com/uc?id=1UgiBF9QLKYFkqr6DsSbozHFjdWoUnWBK',
-    md5='a8f99e911d59deb235fcc1a56bef57e1',
+    url='https://drive.google.com/uc?id=1vnDWKX0ByFwEYssE8W0NNEF_LlL2jqlF',
+    md5='5a85b26f6df5c1c5d9cd5ba838cc493f',
 )
 print(bag_file)
 
--- ros/ros_objslampp_ycb_video/nodes/rosbag_record_rs_rgbd.sh
+++ ros/ros_objslampp_ycb_video/nodes/rosbag_record_rs_rgbd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 
-rosbag record -b 0 /camera/color/camera_info /camera/color/image_rect_color/compressed /camera/aligned_depth_to_color/camera_info /camera/aligned_depth_to_color/image_raw/compressedDepth --output-prefix rs_rgbd $*
+rosbag record -b 0 /camera/color/camera_info /camera/color/image_rect_color /camera/aligned_depth_to_color/camera_info /camera/aligned_depth_to_color/image_raw --output-prefix rs_rgbd $*
