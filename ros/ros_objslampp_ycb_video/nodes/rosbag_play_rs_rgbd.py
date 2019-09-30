13dcd73d2e6d7f6ed15dd891282da441ce430723
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-09-30 11:32:10 +0100

    Update rosbag play

--- ros/ros_objslampp_ycb_video/nodes/rosbag_play_rs_rgbd.py
+++ ros/ros_objslampp_ycb_video/nodes/rosbag_play_rs_rgbd.py
@@ -8,10 +8,9 @@ import rospy
 
 
 bag_file = gdown.cached_download(
-    url='https://drive.google.com/uc?id=1vnDWKX0ByFwEYssE8W0NNEF_LlL2jqlF',
-    md5='5a85b26f6df5c1c5d9cd5ba838cc493f',
+    url='https://drive.google.com/uc?id=12Aa_DWauLHeKokNi5byPwpTeIaRIs8YF',
+    md5='93f0bfd3b7e0a4e3ca00fbe39bbbc587',
 )
-print(bag_file)
 
 cmd = f"rosbag play {bag_file} {' '.join(rospy.myargv()[1:])}"
 print(f'+ {cmd}')
