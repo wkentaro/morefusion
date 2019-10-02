ec623afe8f383d7ab37fa150dc5664e8c4694578
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-10-02 13:13:30 +0100

    Fix /tf /tf_static for rosbag play

--- ros/ros_objslampp_ycb_video/nodes/rosbag_play_rs_rgbd.py
+++ ros/ros_objslampp_ycb_video/nodes/rosbag_play_rs_rgbd.py
@@ -11,7 +11,7 @@ import rospy
 #     url='https://drive.google.com/uc?id=12Aa_DWauLHeKokNi5byPwpTeIaRIs8YF',
 #     md5='93f0bfd3b7e0a4e3ca00fbe39bbbc587',
 # )
-bag_file = '/home/wkentaro/Gdrive/objslampp/ros_objslampp/rs_rgbd_2019-10-02-12-53-31.bag'  # NOQA
+bag_file = '/home/wkentaro/Gdrive/objslampp/ros_objslampp/rs_rgbd_2019-10-02-13-08-40.bag'  # NOQA
 
 cmd = f"rosbag play {bag_file} {' '.join(rospy.myargv()[1:])}"
 print(f'+ {cmd}')
--- ros/ros_objslampp_ycb_video/nodes/rosbag_record_rs_rgbd.sh
+++ ros/ros_objslampp_ycb_video/nodes/rosbag_record_rs_rgbd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 
-rosbag record -b 0 /camera/color/camera_info /camera/color/image_rect_color /camera/aligned_depth_to_color/camera_info /camera/aligned_depth_to_color/image_raw --output-prefix rs_rgbd $*
+rosbag record -b 0 /tf_static /camera/color/camera_info /camera/color/image_rect_color /camera/aligned_depth_to_color/camera_info /camera/aligned_depth_to_color/image_raw --output-prefix rs_rgbd $*
