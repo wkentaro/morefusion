339935d11bce9e511637ea459dbc3c6e90419673
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-10-02 22:16:15 +0100

    Fix MySyntheticYCB20190916RGBDPoseEstimationDatasetReIndexed val

--- objslampp/datasets/rgbd_pose_estimation/my_synthetic_ycb20190916/reindexed.py
+++ objslampp/datasets/rgbd_pose_estimation/my_synthetic_ycb20190916/reindexed.py
@@ -16,7 +16,7 @@ class MySyntheticYCB20190916RGBDPoseEstimationDatasetReIndexed(
         class_ids=None,
         augmentation: bool = False,
     ):
-        self._root_dir = MySyntheticYCB20190916RGBDPoseEstimationDataset('train').root_dir + '.reindexed'  # NOQA
+        self._root_dir = MySyntheticYCB20190916RGBDPoseEstimationDataset(split).root_dir + '.reindexed'  # NOQA
         super().__init__(
             split=split,
             class_ids=class_ids,
@@ -24,6 +24,8 @@ class MySyntheticYCB20190916RGBDPoseEstimationDatasetReIndexed(
         )
 
     def _get_ids(self):
+        assert self.split in ['train', 'val']
+
         image_id_to_instance_ids = collections.defaultdict(list)
         with open(self.root_dir / 'id_to_class_id.json') as f:
             instance_id_to_class_id = json.load(f)
@@ -32,8 +34,15 @@ class MySyntheticYCB20190916RGBDPoseEstimationDatasetReIndexed(
                 image_id_to_instance_ids[image_id].append(instance_id)
         image_id_to_instance_ids = dict(image_id_to_instance_ids)
 
+        image_ids = MySyntheticYCB20190916RGBDPoseEstimationDataset(
+            self.split
+        )._ids
+
         ids = []
-        for image_id, instance_ids in sorted(image_id_to_instance_ids.items()):
+        for image_id in image_ids:
+            if image_id not in image_id_to_instance_ids:
+                continue
+            instance_ids = image_id_to_instance_ids[image_id]
             for instance_id in instance_ids:
                 class_id = instance_id_to_class_id[instance_id]
                 if self._class_ids and class_id not in self._class_ids:
