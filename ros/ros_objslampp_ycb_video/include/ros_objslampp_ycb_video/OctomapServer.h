2cbce2f0adbececa0ced4e3063f279917b225a9e
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-10-05 10:47:20 +0100

    Remove isInUpdateBBX

--- ros/ros_objslampp_ycb_video/include/ros_objslampp_ycb_video/OctomapServer.h
+++ ros/ros_objslampp_ycb_video/include/ros_objslampp_ycb_video/OctomapServer.h
@@ -70,17 +70,6 @@ protected:
       max[i] = std::max(in[i], max[i]);
   };
 
-  /// Test if key is within update area of map (2D, ignores height)
-  inline bool isInUpdateBBX(const OcTreeT::iterator& it) const {
-    // 2^(tree_depth-depth) voxels wide:
-    unsigned voxelWidth = (1 << (m_maxTreeDepth - it.getDepth()));
-    octomap::OcTreeKey key = it.getIndexKey(); // lower corner of voxel
-    return (key[0] + voxelWidth >= m_updateBBXMin[0]
-            && key[1] + voxelWidth >= m_updateBBXMin[1]
-            && key[0] <= m_updateBBXMax[0]
-            && key[1] <= m_updateBBXMax[1]);
-  }
-
   void publishBinaryOctoMap(const ros::Time& rostime = ros::Time::now()) const;
   void publishFullOctoMap(const ros::Time& rostime = ros::Time::now()) const;
   virtual void publishAll(const ros::Time& rostime = ros::Time::now());
