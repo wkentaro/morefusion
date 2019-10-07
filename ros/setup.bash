e302c9eaf3cf2f450fce5722d0fb4fa012e20389
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-10-07 20:19:06 +0100

    Update ros/setup.bash

--- ros/setup.bash
+++ ros/setup.bash
@@ -1,10 +1,14 @@
 OBJSLAMPP_PREFIX=$HOME/ros_objslampp/src/objslampp
 ROSOBJSLAMPP_PREFIX=$HOME/ros_objslampp
 
-unset CMAKE_PREFIX_PATH
-unset PYTHONPATH
+# FIXME: need to load multiple times in some reasons
+for i in seq 0 1; do
 
-source $OBJSLAMPP_PREFIX/.anaconda3/bin/activate
+  unset CMAKE_PREFIX_PATH
+  unset PYTHONPATH
 
-source /opt/ros/kinetic/setup.bash
-source $ROSOBJSLAMPP_PREFIX/devel/setup.bash
+  source $OBJSLAMPP_PREFIX/.anaconda3/bin/activate
+  source /opt/ros/kinetic/setup.bash
+  source $ROSOBJSLAMPP_PREFIX/devel/setup.bash
+
+done
