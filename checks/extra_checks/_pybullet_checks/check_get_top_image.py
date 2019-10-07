44a668985347d81177b623147cd6248cd5efcfd3
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-10-07 11:50:55 +0100

    Add check_get_top_image.py

---
+++ checks/extra_checks/_pybullet_checks/check_get_top_image.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import concurrent.futures
+
+import imgviz
+
+import objslampp
+
+
+def _get_top_image(class_id):
+    models = objslampp.datasets.YCBVideoModels()
+    cad_file = models.get_cad_file(class_id=class_id)
+    return objslampp.extra.pybullet.get_top_image(cad_file)
+
+
+def main():
+    models = objslampp.datasets.YCBVideoModels()
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        futures = []
+        for class_id in range(models.n_class):
+            if class_id == 0:
+                continue
+            future = executor.submit(_get_top_image, class_id)
+            futures.append(future)
+
+    viz = []
+    for future in futures:
+        viz_i = future.result()
+        viz.append(viz_i)
+    viz = imgviz.tile(viz, shape=(4, 6))
+    imgviz.io.pyglet_imshow(viz)
+    imgviz.io.pyglet_run()
+
+
+if __name__ == '__main__':
+    main()
