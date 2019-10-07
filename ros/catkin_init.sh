5fa657e433e86be8b052a2333893102b14f34333
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-10-07 14:51:21 +0100

    Add --blacklist jsk_pcl_ros jsk_pcl_ros_utils

--- ros/catkin_init.sh
+++ ros/catkin_init.sh
@@ -41,6 +41,7 @@ catkin config --merge-devel \
               -DPYTHON_INCLUDE_DIR=$OBJSLAMPP_PREFIX/.anaconda3/include/python3.7m \
               -DPYTHON_LIBRARY=$OBJSLAMPP_PREFIX/.anaconda3/lib/libpython3.7m.so \
               --cmake-args -DCMAKE_BUILD_TYPE=Release
+catkin config --blacklist jsk_pcl_ros jsk_pcl_ros_utils
 mkdir -p $ROSOBJSLAMPP_PREFIX/devel/lib/python3/dist-packages
 ln -fs $OBJSLAMPP_PREFIX/.anaconda3/lib/python3.7/site-packages/cv2 $ROSOBJSLAMPP_PREFIX/devel/lib/python3/dist-packages
 
