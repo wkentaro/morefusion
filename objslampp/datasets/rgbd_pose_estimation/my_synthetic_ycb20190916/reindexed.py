cd6f3a3974cb08b64ecfad82b17b07220c661fdb
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-10-08 10:21:28 +0100

    Add mask data augmentation

--- objslampp/datasets/rgbd_pose_estimation/my_synthetic_ycb20190916/reindexed.py
+++ objslampp/datasets/rgbd_pose_estimation/my_synthetic_ycb20190916/reindexed.py
@@ -16,7 +16,7 @@ class MySyntheticYCB20190916RGBDPoseEstimationDatasetReIndexed(
         class_ids=None,
         augmentation: bool = False,
     ):
-        self._root_dir = MySyntheticYCB20190916RGBDPoseEstimationDataset(split).root_dir + '.reindexed'  # NOQA
+        self._root_dir = MySyntheticYCB20190916RGBDPoseEstimationDataset(split).root_dir + '.reindexed.v2'  # NOQA
         super().__init__(
             split=split,
             class_ids=class_ids,
@@ -27,9 +27,9 @@ class MySyntheticYCB20190916RGBDPoseEstimationDatasetReIndexed(
         assert self.split in ['train', 'val']
 
         image_id_to_instance_ids = collections.defaultdict(list)
-        with open(self.root_dir / 'id_to_class_id.json') as f:
-            instance_id_to_class_id = json.load(f)
-            for instance_id, class_id in instance_id_to_class_id.items():
+        with open(self.root_dir / 'meta.json') as f:
+            instance_id_to_meta = json.load(f)
+            for instance_id, meta in instance_id_to_meta.items():
                 image_id = osp.dirname(instance_id)
                 image_id_to_instance_ids[image_id].append(instance_id)
         image_id_to_instance_ids = dict(image_id_to_instance_ids)
@@ -44,7 +44,8 @@ class MySyntheticYCB20190916RGBDPoseEstimationDatasetReIndexed(
                 continue
             instance_ids = image_id_to_instance_ids[image_id]
             for instance_id in instance_ids:
-                class_id = instance_id_to_class_id[instance_id]
+                meta = instance_id_to_meta[instance_id]
+                class_id = meta['class_id']
                 if self._class_ids and class_id not in self._class_ids:
                     continue
                 ids.append(instance_id)
--- objslampp/datasets/rgbd_pose_estimation/reindexed.py
+++ objslampp/datasets/rgbd_pose_estimation/reindexed.py
@@ -1,7 +1,10 @@
+import cv2
 import imgaug
 import imgaug.augmenters as iaa
+import imgviz
 import numpy as np
 
+from ... import geometry as geometry_module
 from ..base import DatasetBase
 
 
@@ -69,11 +72,89 @@ class RGBDPoseEstimationDatasetReIndexedBase(DatasetBase):
         pcd += random_state.normal(0, 0.003, size=pcd.shape)
         return pcd
 
+    @staticmethod
+    def _augment_mask(rgb, pcd):
+        H, W = rgb.shape[:2]
+        random_state = imgaug.random.get_global_rng()
+        mask = ~np.isnan(pcd).any(axis=2)
+
+        # shift xy
+        case = random_state.choice(4)
+        y1, x1, y2, x2 = geometry_module.masks_to_bboxes([mask])[0]
+        if case == 0:
+            y1 = random_state.uniform(0, H * 0.25)
+        elif case == 1:
+            y2 = H - random_state.uniform(0, H * 0.25)
+        elif case == 2:
+            x1 = random_state.uniform(0, W * 0.25)
+        else:
+            assert case == 3
+            x2 = W - random_state.uniform(0, W * 0.25)
+        y1, x1, y2, x2 = np.array([y1, x1, y2, x2]).round().astype(int)
+        mask[:y1, :] = 0
+        mask[y2:, :] = 0
+        mask[:, :x1] = 0
+        mask[:, x2:] = 0
+
+        # select blobs
+        contours, hierarchy = cv2.findContours(
+            mask.astype(np.uint8),
+            mode=cv2.RETR_TREE,
+            method=cv2.CHAIN_APPROX_SIMPLE,
+        )
+        areas = [cv2.contourArea(c) for c in contours]
+        contour_index = np.argmax(areas)
+        mask_contour = np.zeros((H, W), dtype=np.uint8)
+        cv2.drawContours(
+            mask_contour,
+            contours,
+            contourIdx=contour_index,
+            thickness=-1,
+            color=1,
+        )
+        n_contours = random_state.choice(len(contours))
+        for contour_index in random_state.permutation(
+            len(contours)
+        )[:n_contours]:
+            cv2.drawContours(
+                mask_contour,
+                contours,
+                contourIdx=contour_index,
+                thickness=-1,
+                color=1,
+            )
+        mask = mask_contour.astype(bool)
+
+        rgb[~mask] = 0
+        pcd[~mask] = np.nan
+
+        bbox = geometry_module.masks_to_bboxes([mask])[0]
+        y1, x1, y2, x2 = bbox.round().astype(int)
+
+        rgb = rgb[y1:y2, x1:x2]
+        pcd = pcd[y1:y2, x1:x2]
+        rgb = imgviz.centerize(rgb, (H, W))
+        pcd = imgviz.centerize(
+            pcd, (H, W), cval=np.nan, interpolation='nearest'
+        )
+        return rgb, pcd
+
+    @staticmethod
+    def _augment_rgbd(rgb, pcd):
+        rgb, pcd = RGBDPoseEstimationDatasetReIndexedBase._augment_mask(
+            rgb, pcd
+        )
+        rgb = RGBDPoseEstimationDatasetReIndexedBase._augment_rgb(rgb)
+        pcd = RGBDPoseEstimationDatasetReIndexedBase._augment_pcd(pcd)
+
+        return rgb, pcd
+
     def get_example(self, index):
         id = self._ids[index]
         npz_file = self.root_dir / f'{id}.npz'
         example = dict(np.load(npz_file))
         if self._augmentation:
-            example['rgb'] = self._augment_rgb(example['rgb'])
-            example['pcd'] = self._augment_pcd(example['pcd'])
+            example['rgb'], example['pcd'] = self._augment_rgbd(
+                example['rgb'], example['pcd'],
+            )
         return example
