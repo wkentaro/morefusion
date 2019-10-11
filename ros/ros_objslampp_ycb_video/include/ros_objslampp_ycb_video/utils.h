386ba3d6bb9e642722bcfe2dfe690b8166a17e22
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-10-11 20:37:53 +0100

    Fix for cpplint ros_objslampp_ycb_video/utils.h

--- ros/ros_objslampp_ycb_video/include/ros_objslampp_ycb_video/utils.h
+++ ros/ros_objslampp_ycb_video/include/ros_objslampp_ycb_video/utils.h
@@ -1,5 +1,12 @@
+// Copyright (c) 2019 Kentaro Wada
+
+#ifndef ROS_ROS_OBJSLAMPP_YCB_VIDEO_INCLUDE_ROS_OBJSLAMPP_YCB_VIDEO_UTILS_H_
+#define ROS_ROS_OBJSLAMPP_YCB_VIDEO_INCLUDE_ROS_OBJSLAMPP_YCB_VIDEO_UTILS_H_
+
 #include "ros_objslampp_ycb_video/utils/color.h"
 #include "ros_objslampp_ycb_video/utils/data.h"
 #include "ros_objslampp_ycb_video/utils/geometry.h"
 #include "ros_objslampp_ycb_video/utils/log.h"
 #include "ros_objslampp_ycb_video/utils/opencv.h"
+
+#endif  // ROS_ROS_OBJSLAMPP_YCB_VIDEO_INCLUDE_ROS_OBJSLAMPP_YCB_VIDEO_UTILS_H_
