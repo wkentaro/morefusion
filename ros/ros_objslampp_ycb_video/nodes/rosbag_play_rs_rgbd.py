04ea527d027356e8c6b3e953f3267f7f25cb408d
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-10-20 21:03:51 +0100

    Remove static_wbase

--- ros/ros_objslampp_ycb_video/nodes/rosbag_play_rs_rgbd.py
+++ ros/ros_objslampp_ycb_video/nodes/rosbag_play_rs_rgbd.py
@@ -20,11 +20,6 @@ def get_bag_file(bag_id):
             url='https://drive.google.com/uc?id=1mArVTWl2f0Uws_mRoCtDzttV2s6AahZa',  # NOQA
             md5='a70a792577447a414c8ac7fe5f4aa316',
         )
-    elif bag_id == 'static_wbase':
-        bag_file = gdown.cached_download(
-            url='https://drive.google.com/uc?id=1UxUg4IozQvQNrCALXkzk23v3fBjpFCqZ',  # NOQA
-            md5='3625c11cd130a06557f38b8ff390882e',
-        )
     else:
         raise ValueError(f'Unknown bag_id: {bag_id}')
     return bag_file
