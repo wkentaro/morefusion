2da497170cf7dd8bfdd113b4d884634377b44783
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-10-20 17:08:31 +0100

    Add loginfo with colors

--- objslampp/ros/__init__.py
+++ objslampp/ros/__init__.py
@@ -4,3 +4,11 @@ from .conversions import from_ros_orientation
 from .conversions import from_ros_pose
 from .conversions import from_ros_transform
 from .conversions import from_ros_vector3
+
+from .log import loginfo_blue
+from .log import loginfo_cyan
+from .log import loginfo_red
+from .log import loginfo_green
+from .log import loginfo_yellow
+from .log import loginfo_magenta
+from .log import loginfo_white
---
+++ objslampp/ros/log.py
@@ -0,0 +1,38 @@
+import termcolor
+
+
+def loginfo_red(msg):
+    import rospy
+    rospy.loginfo(termcolor.colored(msg, color='red', attrs={'bold': True}))
+
+
+def loginfo_green(msg):
+    import rospy
+    rospy.loginfo(termcolor.colored(msg, color='green', attrs={'bold': True}))
+
+
+def loginfo_yellow(msg):
+    import rospy
+    rospy.loginfo(termcolor.colored(msg, color='yellow', attrs={'bold': True}))
+
+
+def loginfo_blue(msg):
+    import rospy
+    rospy.loginfo(termcolor.colored(msg, color='blue', attrs={'bold': True}))
+
+
+def loginfo_magenta(msg):
+    import rospy
+    rospy.loginfo(
+        termcolor.colored(msg, color='magenta', attrs={'bold': True})
+    )
+
+
+def loginfo_cyan(msg):
+    import rospy
+    rospy.loginfo(termcolor.colored(msg, color='cyan', attrs={'bold': True}))
+
+
+def loginfo_white(msg):
+    import rospy
+    rospy.loginfo(termcolor.colored(msg, color='white', attrs={'bold': True}))
