be163317eee67779579a9f6299d19d3b9f9c4f79
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-10-20 22:16:46 +0100

    Render cad models based on frame_id

--- ros/ros_objslampp_ycb_video/nodes/render_mesh_markers.py
+++ ros/ros_objslampp_ycb_video/nodes/render_mesh_markers.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pybullet
 import trimesh
+import trimesh.transformations as ttf
 
 import objslampp
 
@@ -10,6 +11,7 @@ import cv_bridge
 import message_filters
 import rospy
 from sensor_msgs.msg import Image, CameraInfo
+import tf
 from topic_tools import LazyTransport
 from visualization_msgs.msg import Marker, MarkerArray
 
@@ -19,6 +21,7 @@ class RenderMeshMarkers(LazyTransport):
     def __init__(self):
         super().__init__()
         self._pub = self.advertise('~output', Image, queue_size=1)
+        self._tf_listener = tf.TransformListener(cache_time=rospy.Duration(30))
         self._post_init()
 
     def subscribe(self):
@@ -38,29 +41,63 @@ class RenderMeshMarkers(LazyTransport):
         for sub in self._subscribers:
             sub.unregister()
 
+    def _get_transform(self, source_frame, target_frame, time):
+        try:
+            self._tf_listener.waitForTransform(
+                target_frame=target_frame,
+                source_frame=source_frame,
+                time=time,
+                timeout=rospy.Duration(0.1),
+            )
+        except Exception as e:
+            rospy.logerr(e)
+            return
+
+        translation, quaternion = self._tf_listener.lookupTransform(
+            target_frame=target_frame,
+            source_frame=source_frame,
+            time=time,
+        )
+        translation = np.asarray(translation)
+        quaternion = np.asarray(quaternion)[[3, 0, 1, 2]]
+        transform = objslampp.functions.transformation_matrix(
+            quaternion, translation
+        ).array
+        return transform
+
     def _callback(self, cam_msg, markers_msg):
         pybullet.connect(pybullet.DIRECT)
 
+        transforms = {}  # (marker's frame_id, stamp): T_marker2cam
         for marker in markers_msg.markers:
             if marker.type != Marker.MESH_RESOURCE:
                 continue
 
+            quaternion, translation = objslampp.ros.from_ros_pose(marker.pose)
+            if marker.header.frame_id != cam_msg.header.frame_id:
+                key = (marker.header.frame_id, marker.header.stamp)
+                if marker.header.frame_id in transforms:
+                    T_marker2cam = transforms[key]
+                else:
+                    T_marker2cam = self._get_transform(
+                        marker.header.frame_id,
+                        cam_msg.header.frame_id,
+                        marker.header.stamp,
+                    )
+                    transforms[key] = T_marker2cam
+                T_cad2marker = objslampp.functions.transformation_matrix(
+                    quaternion, translation
+                ).array
+                T_cad2cam = T_marker2cam @ T_cad2marker
+                quaternion = ttf.quaternion_from_matrix(T_cad2cam)
+                translation = ttf.translation_from_matrix(T_cad2cam)
+            quaternion = quaternion[[1, 2, 3, 0]]
+
             mesh_file = marker.mesh_resource[len('file://'):]
-            position = [
-                marker.pose.position.x,
-                marker.pose.position.y,
-                marker.pose.position.z,
-            ]
-            orientation = [
-                marker.pose.orientation.x,
-                marker.pose.orientation.y,
-                marker.pose.orientation.z,
-                marker.pose.orientation.w,
-            ]
             objslampp.extra.pybullet.add_model(
                 visual_file=mesh_file,
-                position=position,
-                orientation=orientation,
+                position=translation,
+                orientation=quaternion,
                 register=False,
             )
 
