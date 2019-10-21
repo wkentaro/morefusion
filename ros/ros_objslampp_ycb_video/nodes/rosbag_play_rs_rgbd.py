37426c087c8d4a1bd52e302c1eae284080a15fbf
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-10-21 18:13:41 +0100

    Add example_setup_static.desk.launch

--- ros/ros_objslampp_ycb_video/nodes/rosbag_play_rs_rgbd.py
+++ ros/ros_objslampp_ycb_video/nodes/rosbag_play_rs_rgbd.py
@@ -10,12 +10,14 @@ import rospy
 
 
 def get_bag_file(bag_id):
-    if bag_id == 'dynamic':
+    if bag_id == 'dynamic.desk':
         bag_file = gdown.cached_download(
             url='https://drive.google.com/uc?id=1vaPyJERDNFY7W8VUBT3JvPNXHpZ6Bqun',  # NOQA
             md5='5bfb6eb7f80773dd2b8a6b16a93823e9',
         )
-    elif bag_id == 'static':
+    elif bag_id == 'static.robot':
+        bag_file = '/home/wkentaro/Gdrive/objslampp/ros_objslampp/rs_rgbd_2019-10-21-16-56-28.bag'  # NOQA
+    elif bag_id == 'static.desk':
         bag_file = gdown.cached_download(
             url='https://drive.google.com/uc?id=1XPJMTw4VVmUvaMgPXHN0izSWYXRgnhl8',  # NOQA
             md5='4615655dc096d6bad85d15a51a98341b',
@@ -29,7 +31,9 @@ def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument('--id', choices=['dynamic', 'static', 'static_wbase'])
+    parser.add_argument(
+        '--id', choices=['dynamic.desk', 'static.desk', 'static.robot']
+    )
     parser.add_argument('--rosbag-args', nargs=argparse.REMAINDER)
     args = parser.parse_args(rospy.myargv()[1:])
 
