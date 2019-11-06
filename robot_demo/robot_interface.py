d02d3657653f958afceb5b4ca534e9b5ef6cd3f3
djl11 <daniel.lenton11@imperial.ac.uk>
2019-11-06 20:11:59 +0000

    got first full robot demo working.

--- robot_demo/robot_interface.py
+++ robot_demo/robot_interface.py
@@ -3,6 +3,7 @@ import robot_demo.general_kinematics as gk
 import numpy as np
 import rospy
 from std_msgs.msg import Float64MultiArray
+from geometry_msgs.msg import Quaternion, Pose
 from ros_objslampp_srvs.srv import MoveToHome, MoveToPose
 
 
@@ -31,10 +32,10 @@ class RobotInterface:
         self._home_quaternion = gk.hamilton_product(np, q1, q2)
         self.home_pose = np.concatenate((self._home_position, self._home_quaternion), -1)
 
-    def move_to_home(self):
+    def move_to_home(self, velocity_scaling=1.0, acceleration_scaling=1.0):
         rospy.wait_for_service('move_to_home')
         move_to_home = rospy.ServiceProxy('move_to_home', MoveToHome)
-        response = move_to_home()
+        response = move_to_home(velocity_scaling, acceleration_scaling)
         return response.home_reached
 
     # end effector pose #
@@ -52,16 +53,29 @@ class RobotInterface:
 
     # setters
 
-    def set_end_effector_position(self, position):
-        pass
+    def set_end_effector_position(self, position, velocity_scaling=1.0, acceleration_scaling=1.0):
+        rospy.wait_for_service('move_to_position_linearly')
+        move_to_position = rospy.ServiceProxy('move_to_position_linearly', MoveToPose)
+        quaternion = Quaternion(0,0,0,1)
+        pose = Pose()
+        pose.position = position
+        pose.orientation = quaternion
+        response = move_to_position(pose, velocity_scaling, acceleration_scaling)
+        return response.pose_reached
 
     def set_end_effector_quaternion(self, quaternion):
         pass
 
-    def set_end_effector_quaternion_pose(self, pose):
-        rospy.wait_for_service('move_to_pose')
-        move_to_pose = rospy.ServiceProxy('move_to_pose', MoveToPose)
-        response = move_to_pose(pose)
+    def set_end_effector_quaternion_pose(self, pose, velocity_scaling=1.0, acceleration_scaling=1.0):
+        rospy.wait_for_service('move_to_pose_linearly')
+        move_to_pose = rospy.ServiceProxy('move_to_pose_linearly', MoveToPose)
+        response = move_to_pose(pose, velocity_scaling, acceleration_scaling)
+        return response.pose_reached
+
+    def set_end_effector_quaternion_pointing_pose(self, pose, velocity_scaling=1.0, acceleration_scaling=1.0):
+        rospy.wait_for_service('/pointing_pose_service/move_to_pointing_pose')
+        move_to_pose = rospy.ServiceProxy('/pointing_pose_service/move_to_pointing_pose', MoveToPose)
+        response = move_to_pose(pose, velocity_scaling, acceleration_scaling)
         return response.pose_reached
 
     # joint angles #
