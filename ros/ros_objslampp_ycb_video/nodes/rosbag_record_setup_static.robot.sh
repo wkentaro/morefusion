2fc772fdf342a5888327a9d6d69fbd0b57967905
djl11 <daniel.lenton11@imperial.ac.uk>
2019-11-12 23:34:56 +0000

    Add rosbag record script for demo

---
+++ ros/ros_objslampp_ycb_video/nodes/rosbag_record_setup_static.robot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+
+rosbag record -b 0 \
+  /tf \
+  /joint_states \
+  /joint_angles \
+  /tf_static_republished \
+  /camera/color/camera_info \
+  /camera/color/image_rect_color \
+  /camera/aligned_depth_to_color/camera_info \
+  /camera/aligned_depth_to_color/image_raw \
+  /camera/free_cells_vis_array \
+  /camera/mask_rcnn_instance_segmentation/output/class \
+  /camera/mask_rcnn_instance_segmentation/output/label_ins \
+  /camera/octomap_server/output/class \
+  /camera/octomap_server/output/grids \
+  /camera/octomap_server/output/grids_for_render \
+  /camera/octomap_server/output/grids_noentry \
+  /camera/octomap_server/output/label_rendered \
+  /camera/octomap_server/output/label_tracked \
+  /camera/octomap_server/output/markers_bg \
+  /camera/octomap_server/output/markers_fg \
+  /camera/select_picking_order/output/graph \
+  /camera/select_picking_order/output/poses \
+  /camera/select_picking_order/output/poses_viz \
+  /camera/with_occupancy/collision_based_pose_refinement/object_mapping/output/poses \
+  /camera/with_occupancy/collision_based_pose_refinement/output \
+  /camera/with_occupancy/object_mapping/output/poses \
+  /camera/with_occupancy/singleview_3d_pose_estimation/output \
+  /camera/with_occupancy/singleview_3d_pose_estimation/output/debug/rgbd \
+  --output-prefix setup_static.robot $*
