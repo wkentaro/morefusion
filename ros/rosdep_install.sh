d973309965d9927ba3ef40ab002c1d209e4e1f59
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-12-09 20:18:02 +0000

    franka_description_custom/** -> ros_objslampp_panda

--- ros/rosdep_install.sh
+++ ros/rosdep_install.sh
@@ -12,5 +12,5 @@ $ROSOBJSLAMPP_PREFIX/src/objslampp/ros/install_realsense.sh
 rosdep init
 rosdep update
 
-rosdep install --from-path src/objslampp/ros --skip-keys 'realsense2_camera orb_slam2_ros franka_description_custom'
+rosdep install --from-path src/objslampp/ros --skip-keys 'realsense2_camera orb_slam2_ros ros_objslampp_panda'
 rosdep install --from-path src/jsk-ros-pkg/jsk_recognition/jsk_recognition_utils
