d539ff5bc2bd0962417b956279bc13c4d8296b79
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-12-10 12:13:11 +0000

    Fix package.xml and catkin_build.robot_node.sh

--- ros/catkin_build.robot_node.sh
+++ ros/catkin_build.robot_node.sh
@@ -22,18 +22,46 @@ fi
 
 catkin config --merge-devel \
               --cmake-args -DCMAKE_BUILD_TYPE=Release
-catkin config --whitelist \
-  franka_control \
-  franka_description \
-  franka_example_controllers \
-  franka_gripper \
-  franka_hw \
-  franka_msgs \
-  franka_ros \
-  franka_visualization \
-  ros_objslampp_panda
+catkin config --blacklist \
+  checkerboard_detector \
+  imagesift \
+  image_view2 \
+  jsk_network_tools \
+  jsk_pcl_ros \
+  jsk_pcl_ros_utils \
+  jsk_perception \
+  jsk_recognition_msgs \
+  jsk_tools \
+  message_filters \
+  rosbag \
+  rosbag_storage \
+  rosconsole \
+  roscpp \
+  rosgraph \
+  roslaunch \
+  roslz4 \
+  rosmaster \
+  rosmsg \
+  rosmsg \
+  rosnode \
+  rosout \
+  rosparam \
+  rospy \
+  rosservice \
+  rostest \
+  rostopic \
+  roswtf \
+  tf \
+  tf2 \
+  tf2_eigen \
+  tf2_geometry_msgs \
+  tf2_msgs \
+  tf2_py \
+  tf2_ros \
+  topic_tools \
+  xmlrpcpp
 
-catkin build
+catkin build ros_objslampp_panda
 
 set +x
 source /opt/ros/kinetic/setup.bash
