204bce46cdde344d84704d36a6ea9cf2f459ee8d
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-12-10 13:27:32 +0000

    ros_objslampp_XXX -> morefusion_XXX

--- ros/catkin_build.robot_agent.sh
+++ ros/catkin_build.robot_agent.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-OBJSLAMPP_PREFIX=$HOME/ros_objslampp/src/objslampp
-ROSOBJSLAMPP_PREFIX=$HOME/ros_objslampp
+PY_ROOT_PREFIX=$HOME/ros_morefusion/src/objslampp
+ROS_ROOT_PREFIX=$HOME/ros_morefusion
 
-if [ ! -d $OBJSLAMPP_PREFIX ]; then
-  echo "Please install objslampp to $OBJSLAMPP_PREFIX"
+if [ ! -d $PY_ROOT_PREFIX ]; then
+  echo "Please install objslampp to $PY_ROOT_PREFIX"
   exit 1
 fi
 
-if [ ! -e $OBJSLAMPP_PREFIX/.anaconda3/bin/activate ]; then
+if [ ! -e $PY_ROOT_PREFIX/.anaconda3/bin/activate ]; then
   echo "Please run 'make install' in objslampp"
   exit 1
 fi
@@ -18,7 +18,7 @@ fi
 unset PYTHONPATH
 unset CMAKE_PREFIX_PATH
 
-source $OBJSLAMPP_PREFIX/.anaconda3/bin/activate
+source $PY_ROOT_PREFIX/.anaconda3/bin/activate
 source /opt/ros/kinetic/setup.bash
 
 set -x
@@ -28,26 +28,26 @@ pip install rospkg
 pip install empy
 pip install netifaces
 
-mkdir -p $ROSOBJSLAMPP_PREFIX/src
-cd $ROSOBJSLAMPP_PREFIX
+mkdir -p $ROS_ROOT_PREFIX/src
+cd $ROS_ROOT_PREFIX
 catkin init
 
-if [ ! -e $ROSOBJSLAMPP_PREFIX/src/.rosinstall ]; then
-  ln -s $ROSOBJSLAMPP_PREFIX/src/objslampp/ros/rosinstall $ROSOBJSLAMPP_PREFIX/src/.rosinstall
-  (cd $ROSOBJSLAMPP_PREFIX/src && wstool up)
+if [ ! -e $ROS_ROOT_PREFIX/src/.rosinstall ]; then
+  ln -s $ROS_ROOT_PREFIX/src/objslampp/ros/rosinstall $ROS_ROOT_PREFIX/src/.rosinstall
+  (cd $ROS_ROOT_PREFIX/src && wstool up)
 fi
 
-if [ ! -e $ROSOBJSLAMPP_PREFIX/.autoenv.zsh ]; then
-  cp $OBJSLAMPP_PREFIX/ros/template.autoenv.zsh $ROSOBJSLAMPP_PREFIX/.autoenv.zsh
+if [ ! -e $ROS_ROOT_PREFIX/.autoenv.zsh ]; then
+  cp $PY_ROOT_PREFIX/ros/template.autoenv.zsh $ROS_ROOT_PREFIX/.autoenv.zsh
 fi
-if [ ! -e $ROSOBJSLAMPP_PREFIX/.autoenv_leave.zsh ]; then
-  cp $OBJSLAMPP_PREFIX/ros/template.autoenv_leave.zsh $ROSOBJSLAMPP_PREFIX/.autoenv_leave.zsh
+if [ ! -e $ROS_ROOT_PREFIX/.autoenv_leave.zsh ]; then
+  cp $PY_ROOT_PREFIX/ros/template.autoenv_leave.zsh $ROS_ROOT_PREFIX/.autoenv_leave.zsh
 fi
 
 catkin config --merge-devel \
-              -DPYTHON_EXECUTABLE=$OBJSLAMPP_PREFIX/.anaconda3/bin/python \
-              -DPYTHON_INCLUDE_DIR=$OBJSLAMPP_PREFIX/.anaconda3/include/python3.7m \
-              -DPYTHON_LIBRARY=$OBJSLAMPP_PREFIX/.anaconda3/lib/libpython3.7m.so \
+              -DPYTHON_EXECUTABLE=$PY_ROOT_PREFIX/.anaconda3/bin/python \
+              -DPYTHON_INCLUDE_DIR=$PY_ROOT_PREFIX/.anaconda3/include/python3.7m \
+              -DPYTHON_LIBRARY=$PY_ROOT_PREFIX/.anaconda3/lib/libpython3.7m.so \
               --cmake-args -DCMAKE_BUILD_TYPE=Release -DOCTOMAP_OMP=1
 catkin config --blacklist \
   checkerboard_detector \
@@ -63,18 +63,18 @@ catkin config --blacklist \
   rosbag_storage \
   franka_control \
   franka_visualization
-mkdir -p $ROSOBJSLAMPP_PREFIX/devel/lib/python3/dist-packages
-ln -fs $OBJSLAMPP_PREFIX/.anaconda3/lib/python3.7/site-packages/cv2 $ROSOBJSLAMPP_PREFIX/devel/lib/python3/dist-packages
+mkdir -p $ROS_ROOT_PREFIX/devel/lib/python3/dist-packages
+ln -fs $PY_ROOT_PREFIX/.anaconda3/lib/python3.7/site-packages/cv2 $ROS_ROOT_PREFIX/devel/lib/python3/dist-packages
 
 catkin build cv_bridge
 
 set +x
-source $OBJSLAMPP_PREFIX/.anaconda3/bin/activate
+source $PY_ROOT_PREFIX/.anaconda3/bin/activate
 source /opt/ros/kinetic/setup.bash
-source $ROSOBJSLAMPP_PREFIX/devel/setup.bash
+source $ROS_ROOT_PREFIX/devel/setup.bash
 set -x
 
 python -c 'import cv2'
 python -c 'from cv_bridge.boost.cv_bridge_boost import getCvType'
 
-catkin build ros_objslampp_ycb_video
+catkin build morefusion_ycb_video
