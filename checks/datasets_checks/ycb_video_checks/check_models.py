13f7ad30c6af87d9ad1fdd95a48e830bf2557616
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-12-11 15:20:07 +0000

    Use display_scenes in check_models.py

--- checks/datasets_checks/ycb_video_checks/check_models.py
+++ checks/datasets_checks/ycb_video_checks/check_models.py
@@ -18,17 +18,28 @@ def main():
     models = morefusion.datasets.YCBVideoModels()
     class_names = morefusion.datasets.ycb_video.class_names
 
-    cads = []
+    cads = {}
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for class_id, class_name in enumerate(class_names):
             if class_id == 0:
                 continue
             result = executor.submit(get_uniform_scale_cad, models, class_id)
-            cads.append(result)
-    cads = [future.result() for future in cads]
+            cads[f'{class_id:04d}'] = result
 
-    scene = morefusion.extra.trimesh.tile_meshes(cads)
-    scene.show()
+    scenes = {}
+    camera_transform = None
+    for k, v in cads.items():
+        cad = v.result()
+        scene = cad.scene()
+        if camera_transform is None:
+            camera_transform = scene.camera_transform
+        else:
+            scene.camera_transform = camera_transform
+        scenes[k] = scene
+
+    morefusion.extra.trimesh.display_scenes(
+        scenes, height=480 // 3, width=640 // 3, tile=(3, 7)
+    )
 
 
 if __name__ == '__main__':
