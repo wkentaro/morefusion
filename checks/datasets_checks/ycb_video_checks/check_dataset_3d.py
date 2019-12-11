6a9f9de9bc6cde478a81ba12290b07b9e55584f5
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-12-11 14:21:44 +0000

    Use display_scenes in check_dataset_3d.py

--- checks/datasets_checks/ycb_video_checks/check_dataset_3d.py
+++ checks/datasets_checks/ycb_video_checks/check_dataset_3d.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import numpy as np
-import pyglet
 import trimesh
 import trimesh.viewer
 import trimesh.transformations as tf
@@ -9,74 +8,60 @@ import trimesh.transformations as tf
 import morefusion
 
 
-def callback(scene):
-    if not scene.play:
-        return
-
+def get_scenes():
     dataset = morefusion.datasets.YCBVideoDataset('train')
-    example = dataset[scene.index]
 
-    video_id, frame_id = dataset.ids[scene.index].split('/')
+    index = 0
+    video_id_prev = None
+    scene = trimesh.Scene()
+    while index < len(dataset):
+        example = dataset[index]
 
-    # Reset scene for new scene.
-    if video_id != scene.video_id:
-        scene.geometry = {}
-        scene.graph.clear()
-    scene.video_id = video_id
+        video_id, frame_id = dataset.ids[index].split('/')
 
-    rgb = example['color']
-    depth = example['depth']
-    K = example['meta']['intrinsic_matrix']
+        clear = False
+        if video_id_prev is not None and video_id_prev != video_id:
+            clear = True
+            for node_name in scene.graph.nodes_geometry:
+                scene.graph.transforms.remove_node(node_name)
+        video_id_prev = video_id
 
-    T_world2cam = np.r_[
-        example['meta']['rotation_translation_matrix'], [[0, 0, 0, 1]]
-    ]
-    T_cam2world = np.linalg.inv(T_world2cam)
-    pcd = morefusion.geometry.pointcloud_from_depth(
-        depth, fx=K[0, 0], fy=K[1, 1], cx=K[0, 2], cy=K[1, 2]
-    )
-    nonnan = ~np.isnan(depth)
-    geom = trimesh.PointCloud(vertices=pcd[nonnan], colors=rgb[nonnan])
-    scene.add_geometry(geom, transform=T_cam2world)
+        rgb = example['color']
+        depth = example['depth']
+        K = example['meta']['intrinsic_matrix']
 
-    # A kind of current camera view, but a bit far away to see whole scene.
-    scene.camera.resolution = (rgb.shape[1], rgb.shape[0])
-    scene.camera.focal = (K[0, 0], K[1, 1])
-    scene.camera.transform = morefusion.extra.trimesh.to_opengl_transform(
-        T_cam2world @ tf.translation_matrix([0, 0, -0.5])
-    )
-    # scene.set_camera()
+        T_world2cam = np.r_[
+            example['meta']['rotation_translation_matrix'], [[0, 0, 0, 1]]
+        ]
+        T_cam2world = np.linalg.inv(T_world2cam)
+        pcd = morefusion.geometry.pointcloud_from_depth(
+            depth, fx=K[0, 0], fy=K[1, 1], cx=K[0, 2], cy=K[1, 2]
+        )
+        nonnan = ~np.isnan(depth)
+        geom = trimesh.PointCloud(vertices=pcd[nonnan], colors=rgb[nonnan])
+        scene.add_geometry(geom, transform=T_cam2world)
 
-    scene.index += 15
+        # A kind of current camera view, but a bit far away to see whole scene.
+        scene.camera.resolution = (rgb.shape[1], rgb.shape[0])
+        scene.camera.focal = (K[0, 0], K[1, 1])
+        scene.camera_transform = morefusion.extra.trimesh.to_opengl_transform(
+            T_cam2world @ tf.translation_matrix([0, 0, -0.5])
+        )
+        # scene.set_camera()
+
+        geom = trimesh.creation.camera_marker(
+            scene.camera, marker_height=0.05
+        )[1]
+        geom.colors = (0, 1., 0)
+        scene.add_geometry(geom, transform=T_cam2world)
+
+        index += 15
+        print(f'[{index:08d}] video_id={video_id}')
+        yield {'__clear__': clear, 'rgb': rgb, 'scene': scene}
 
 
 def main():
-    scene = trimesh.Scene()
-    scene.index = 0
-    scene.video_id = None
-    scene.play = False
-
-    # To avoid error for empty scene.
-    # We register big cube for the far parameter of rendering.
-    dummy_geom = trimesh.creation.box((1, 1, 1))
-    dummy_geom.visual.face_colors = (0, 0, 0, 0)
-    scene.add_geometry(dummy_geom)
-
-    scene.camera.resolution = (640, 480)
-
-    window = trimesh.viewer.SceneViewer(
-        scene, callback=callback, start_loop=False
-    )
-
-    @window.event
-    def on_key_press(symbol, modifiers):
-        if modifiers == 0:
-            if symbol == pyglet.window.key.S:
-                scene.play = not scene.play
-
-    print('Press S key to start/pause.')
-
-    pyglet.app.run()
+    morefusion.extra.trimesh.display_scenes(get_scenes(), tile=(1, 2))
 
 
 if __name__ == '__main__':
