ad79d8bffc6bfb66d1a2de9f762e189a1bb9935b
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-12-12 17:02:39 +0000

    Fix box_outline_from_voxel(_grid)

--- checks/extra_checks/_trimesh_checks/check_box_outline_from_voxel_grid.py
+++ checks/extra_checks/_trimesh_checks/check_box_outline_from_voxel_grid.py
@@ -7,10 +7,10 @@ import morefusion
 
 models = morefusion.datasets.YCBVideoModels()
 
-voxel = models.get_solid_voxel_grid(class_id=1)
+vg = models.get_solid_voxel_grid(class_id=1)
 
 scene = trimesh.Scene()
-scene.add_geometry(voxel.as_boxes(colors=(0, 127, 0)))
-scene.add_geometry(morefusion.extra.trimesh.box_outline_from_voxel(voxel))
+scene.add_geometry(vg.as_boxes(colors=(0, 127, 0)))
+scene.add_geometry(morefusion.extra.trimesh.box_outline_from_voxel_grid(vg))
 scene.add_geometry(trimesh.creation.axis(origin_size=0.01))
 morefusion.extra.trimesh.display_scenes({'scene': scene})
---
+++ morefusion/extra/_trimesh/box_outline_from_voxel_grid.py
@@ -0,0 +1,12 @@
+import numpy as np
+import trimesh
+
+
+def box_outline_from_voxel_grid(vg):
+    assert isinstance(vg, trimesh.voxel.VoxelGrid)
+    geom = trimesh.path.creation.box_outline(
+        np.array(vg.shape) * vg.scale
+    )
+    geom.apply_translation((np.array(vg.shape) / 2.0 - 0.5) * vg.scale)
+    geom.apply_translation(vg.origin)
+    return geom
