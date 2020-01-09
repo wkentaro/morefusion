4d21bcd86a0da05c58c4de7483be480d8789c0c7
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-01-09 15:28:38 +0000

    Update ros/rosdep_install.sh

--- ros/rosdep_install.sh
+++ ros/rosdep_install.sh
@@ -9,8 +9,10 @@ set -x
 
 $ROS_ROOT_PREFIX/src/morefusion/ros/install_realsense.sh
 
-rosdep init
+if [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
+  sudo rosdep init
+fi
 rosdep update
 
 rosdep install --from-path src/morefusion/ros --skip-keys 'realsense2_camera orb_slam2_ros morefusion_panda'
-rosdep install --from-path src/jsk-ros-pkg/jsk_recognition/jsk_recognition_utils
+rosdep install --from-path src/jsk-ros-pkg/jsk_recognition/jsk_recognition_utils --skip-keys 'python-chainer-pip'
