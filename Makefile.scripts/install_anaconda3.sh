242b980eb99a4db7506a4acc3dc0bc905433132c
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-08-15 16:25:00 +0100

    Install Python=3.7

--- Makefile.scripts/install_anaconda3.sh
+++ Makefile.scripts/install_anaconda3.sh
@@ -7,3 +7,6 @@ if [ ! -d $ROOT/.anaconda3 ]; then
   cd $ROOT
   curl -L https://github.com/wkentaro/dotfiles/raw/master/local/bin/install_anaconda3.sh | bash -s .
 fi
+
+source $ROOT/.anaconda3/bin/activate
+conda install python=3.7 -y -q
