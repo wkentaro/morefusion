488aa1e9ae1fd72d691f537271e71db58a144081
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-10-25 11:16:15 +0100

    Stop downloading install_anaconda3.sh from wkentaro/dotfiles

--- Makefile.scripts/install_anaconda3.sh
+++ Makefile.scripts/install_anaconda3.sh
@@ -1,12 +1,41 @@
-#!/bin/bash
+#!/bin/bash -e
 
-HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROOT=$HERE/..
+HERE=$(realpath $(dirname ${BASH_SOURCE[0]}))
+source $HERE/__init__.sh
+ROOT=$(realpath $HERE/..)
 
-if [ ! -d $ROOT/.anaconda3 ]; then
-  cd $ROOT
-  curl -L https://github.com/wkentaro/dotfiles/raw/master/local/bin/install_anaconda3.sh | bash -s .
+INSTALL_DIR=$ROOT
+
+if [ -e $INSTALL_DIR/.anaconda3 ]; then
+  echo_bold "==> Anaconda3 is already installed: $INSTALL_DIR/.anaconda3"
+  exit 0
 fi
 
-source $ROOT/.anaconda3/bin/activate
-conda install python=3.7 -y -q
+echo_bold "==> Installing Anaconda3 to: $INSTALL_DIR/.anaconda3"
+
+TMPDIR=$(mktemp -d)
+cd $TMPDIR
+
+if [ "$(uname)" = "Linux" ]; then
+  URL='https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh'
+elif [ "$(uname)" = "Darwin" ]; then
+  URL='https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh'
+else
+  echo_warning "==> Unsupported platform: $(uname)"
+  exit 1
+fi
+
+if which wget &>/dev/null; then
+  wget -q $URL -O miniconda3.sh
+else
+  curl -s -L $URL -o miniconda3.sh
+fi
+
+unset PYTHONPATH
+
+bash ./miniconda3.sh -p $INSTALL_DIR/.anaconda3 -b
+cd -
+rm -rf $TMPDIR
+
+source $INSTALL_DIR/.anaconda3/bin/activate
+conda_install 'python<3.9'  # for open3d-python
